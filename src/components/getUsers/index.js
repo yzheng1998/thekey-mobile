dcbbb686c2415d4c69965ee8d19146d3fe8ba4a6
Michele Wang <mwang@Michele-Wangs-MacBook-Pro.local>
2018-06-04 16:51:34 -0400

    verified the query works

--- src/components/getUsers/index.js
+++ src/components/getUsers/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import gql from 'graphql-tag'
+import { Query } from 'react-apollo'
+import { Text, View } from 'react-native'
 
 const GET_USERS = gql`
   query users {
@@ -11,21 +13,18 @@ const GET_USERS = gql`
     }
   }
 `
-
-class getUsers extends Component {
+const message = <Text>please</Text>
+class GetUsers extends Component {
   render() {
-    return(
-      <div>
-        <Query query = {GET_USERS}>
-        {({ loading, error, data }) => {
-           if (loading) return 'Loading...'
-           if (error) return `Error! ${error.message}`
-           data.users.map(user => (
-             <div>{user.id}</div>
-           ))
-        }
-      }
-      </div>
+    return (
+      <Query query={GET_USERS}>
+        {({ loading, data }) => {
+          const text = JSON.stringify(data.users)
+          return <Text>{text}</Text>
+        }}
+      </Query>
     )
   }
 }
+
+export default GetUsers
