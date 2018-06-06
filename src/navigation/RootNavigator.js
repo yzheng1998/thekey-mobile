21751c0ad14c5c8b9f97200372a1541fa7505f30
Michele Wang <mwang@Michele-Wangs-MacBook-Pro.local>
2018-06-06 10:47:12 -0400

    Added EventsView to root navigator

--- src/navigation/RootNavigator.js
+++ src/navigation/RootNavigator.js
@@ -1,11 +1,27 @@
 import { createStackNavigator } from 'react-navigation'
 
 import MainScreen from '../screens/MainScreen/index'
+import RegisterScreen from '../screens/RegisterScreen/'
+import UserScreen from '../screens/UserScreen/'
+import UsersScreen from '../screens/UsersScreen/'
+import EventsScreen from '../screens/EventsScreen/'
 
 const RootNavigator = createStackNavigator(
   {
     Main: {
       screen: MainScreen,
+    },
+    Register: {
+      screen: RegisterScreen,
+    },
+    Users: {
+      screen: UsersScreen,
+    },
+    User: {
+      screen: UserScreen,
+    },
+    Events: {
+      screen: EventsScreen,
     },
   },
   {
