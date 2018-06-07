719f8109bd7f553f4311361fe1c03b9b37d813dc
Michele Wang <mwang@Michele-Wangs-MacBook-Pro.local>
2018-06-07 17:22:41 -0400

    Added friend request list view screen

---
+++ src/screens/FriendRequestScreen/index.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react'
+import gql from 'graphql-tag'
+import { Query } from 'react-apollo'
+import { FlatList } from 'react-native'
+import FriendRequestListItem from './FriendRequestListItem'
+
+const GET_FRIEND_REQUESTS = gql`
+  query viewer {
+    viewer {
+      id
+      friendRequests {
+        id
+        sender {
+          id
+          firstName
+          lastName
+        }
+      }
+    }
+  }
+`
+
+class FriendRequestScreen extends Component {
+  render() {
+    return (
+      <Query query={GET_FRIEND_REQUESTS}>
+        {({ loading, error, data }) => {
+          if (loading) return 'Loading...'
+          if (error) return `Error! ${error.message}`
+          return (
+            <FlatList
+              keyExtractor={friendRequest => friendRequest.id}
+              data={data.viewer.friendRequests}
+              renderItem={({ item: friendRequest }) => (
+                <FriendRequestListItem
+                  title={`${friendRequest.sender.firstName} ${
+                    friendRequest.sender.lastName
+                  }`}
+                  friendRequestID={friendRequest.id}
+                />
+              )}
+            />
+          )
+        }}
+      </Query>
+    )
+  }
+}
+
+export default FriendRequestScreen
