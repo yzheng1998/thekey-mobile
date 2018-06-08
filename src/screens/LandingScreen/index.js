b05697302d04c9a7d88f71efcf5d5ef3c9975bbd
Michele Wang <mwang@Michele-Wangs-MacBook-Pro.local>
2018-06-08 11:37:57 -0400

    Added landing screen view with routes to login/apply. also included link to main page for testing

---
+++ src/screens/LandingScreen/index.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+import { Button } from 'react-native-elements'
+
+class LandingScreen extends Component {
+  render() {
+    return (
+      <View>
+        <Text>The Key</Text>
+        <Button
+          title="Login"
+          onPress={() => this.props.navigation.navigate('Login')}
+        />
+        <Button
+          title="Apply"
+          onPress={() => this.props.navigation.navigate('Register')}
+        />
+        <Button
+          // For testing the rest of the app bc don't have login functionality yet
+          title="Main Page (for testing)"
+          onPress={() => this.props.navigation.navigate('Main')}
+        />
+      </View>
+    )
+  }
+}
+
+export default LandingScreen
