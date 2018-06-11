b340e941f8c918cfbbdaaabf2942ca38a5caa8c4
Shahd Nara <snara@Shahd-Naras-MacBook-Pro.local>
2018-06-11 15:18:48 -0400

    Creates login screen

--- src/screens/LoginScreen/index.js
+++ src/screens/LoginScreen/index.js
@@ -1,17 +1,77 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
-import { Button } from 'react-native-elements'
+import { FormLabel, FormInput } from 'react-native-elements'
+import gql from 'graphql-tag'
+import { Mutation } from 'react-apollo'
+import { Button, AsyncStorage, View } from 'react-native'
+import { Message } from './styles'
 
+const LOGIN_USER = gql`
+  mutation loginUser($email: String!, $password: String!) {
+    loginUser(email: $email, password: $password) {
+      token
+      error {
+        message
+      }
+    }
+  }
+`
 class LoginScreen extends Component {
+  state = {
+    email: '',
+    password: '',
+  }
+
   render() {
     return (
-      <View>
-        <Text>Temporary Login Screen</Text>
-        <Button
-          title="Go on to main screen as if you logged in"
-          onPress={() => this.props.navigation.navigate('MainTab')}
-        />{' '}
-      </View>
+      <Mutation
+        mutation={LOGIN_USER}
+        onCompleted={async data => {
+          const {
+            loginUser: { token },
+          } = data
+
+          if (!data.loginUser.error) {
+            await (token && AsyncStorage.setItem('token', token))
+            this.props.navigation.navigate('MainTab')
+          }
+        }}
+      >
+        {(loginUser, { loading, data, error }) => (
+          <View>
+            <FormLabel>Email:</FormLabel>
+            <FormInput
+              onChangeText={newText => this.setState({ email: newText })}
+              type="text"
+              name="email"
+            />
+            <FormLabel>Password:</FormLabel>
+            <FormInput
+              onChangeText={newText => this.setState({ password: newText })}
+              type="password"
+              name="password"
+            />
+
+            {loading && <Message>Logging you in...</Message>}
+            {data &&
+              data.loginUser.error && (
+                <Message>{data.loginUser.error.message}</Message>
+              )}
+            {error && <Message>Server error</Message>}
+
+            <Button
+              title="Login"
+              disabled={loading}
+              onPress={() => {
+                const variables = {
+                  email: this.state.email,
+                  password: this.state.password,
+                }
+                loginUser({ variables })
+              }}
+            />
+          </View>
+        )}
+      </Mutation>
     )
   }
 }
