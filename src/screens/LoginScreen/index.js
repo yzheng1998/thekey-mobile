423949ff9884fc72a16ac81da6a0de1edb8ed4ac
Michele Wang <mwang@Michele-Wangs-MacBook-Pro.local>
2018-06-11 11:04:09 -0400

    added path to MainTab from login

--- src/screens/LoginScreen/index.js
+++ src/screens/LoginScreen/index.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
 import { Text, View } from 'react-native'
+import { Button } from 'react-native-elements'
 
 class LoginScreen extends Component {
   render() {
     return (
       <View>
         <Text>Temporary Login Screen</Text>
+        <Button
+          title="Go on to main screen as if you logged in"
+          onPress={() => this.props.navigation.navigate('MainTab')}
+        />{' '}
       </View>
     )
   }
