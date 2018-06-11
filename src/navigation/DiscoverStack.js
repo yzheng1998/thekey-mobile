de7f35950547a78c26bc2ea51d34ae3ad303717e
Michele Wang <mwang@Michele-Wangs-MacBook-Pro.local>
2018-06-11 11:00:13 -0400

    Added DiscoverStack: stack for Jobs, Events, Users

---
+++ src/navigation/DiscoverStack.js
@@ -0,0 +1,69 @@
+import { createStackNavigator } from 'react-navigation'
+import UsersScreen from '../screens/UsersScreen'
+import UserScreen from '../screens/UserScreen'
+import EventsScreen from '../screens/EventsScreen'
+import EventScreen from '../screens/EventScreen'
+import JobsScreen from '../screens/JobsScreen'
+import JobScreen from '../screens/JobScreen'
+import DiscoverScreen from '../screens/DiscoverScreen'
+import FriendRequestScreen from '../screens/FriendRequestScreen'
+
+const DiscoverStack = createStackNavigator(
+  {
+    Discover: {
+      screen: DiscoverScreen,
+      navigationOptions: () => ({
+        title: 'View Options',
+      }),
+    },
+    Users: {
+      screen: UsersScreen,
+      navigationOptions: () => ({
+        title: 'View Users',
+      }),
+    },
+    User: {
+      screen: UserScreen,
+      navigationOptions: () => ({
+        title: 'View User',
+      }),
+    },
+    Events: {
+      screen: EventsScreen,
+      navigationOptions: () => ({
+        title: 'View Events',
+      }),
+    },
+    Event: {
+      screen: EventScreen,
+      navigationOptions: () => ({
+        title: 'View Event',
+      }),
+    },
+    Jobs: {
+      screen: JobsScreen,
+      navigationOptions: () => ({
+        title: 'View Jobs',
+      }),
+    },
+    Job: {
+      screen: JobScreen,
+      navigationOptions: () => ({
+        title: 'View Jobs',
+      }),
+    },
+    FriendRequests: {
+      screen: FriendRequestScreen,
+      navigationOptions: () => ({
+        title: 'View Friend Requests',
+      }),
+    },
+  },
+  {
+    initialRouteName: 'Discover',
+    backBehavior: 'Discover',
+    headerMode: 'screen',
+  },
+)
+
+export default DiscoverStack
