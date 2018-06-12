ca82602f1e429da155256362275e044dc67cb34c
Shahd Nara <snara@Shahd-Naras-MacBook-Pro.local>
2018-06-12 14:34:41 -0400

    Added LinkedIn to Login Screen

--- src/screens/LoginScreen/index.js
+++ src/screens/LoginScreen/index.js
@@ -1,23 +1,84 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
-import { Button } from 'react-native-elements'
-import LinkedInModal from 'react-native-linkedin'
+import LinkedInLoginButton from './components/LinkedInLoginButton'
+import { FormLabel, FormInput } from 'react-native-elements'
+import gql from 'graphql-tag'
+import { Mutation } from 'react-apollo'
+import { Button, AsyncStorage, View } from 'react-native'
+import { Message } from './styles'
 
+const LOGIN_USER = gql`
+  mutation loginUser($email: String!, $password: String!) {
+    loginUser(email: $email, password: $password) {
+      user {
+        id
+        firstName
+      }
+      token
+      error {
+        message
+      }
+    }
+  }
+`
 class LoginScreen extends Component {
+  state = {
+    email: '',
+    password: '',
+  }
+
   render() {
     return (
       <View>
-        <LinkedInModal
-          clientID="[ Your client id from https://www.linkedin.com/developer/apps ]"
-          clientSecret="[ Your client secret from https://www.linkedin.com/developer/apps ]"
-          redirectUri="[ Your redirect uri set into https://www.linkedin.com/developer/apps ]"
-          onSuccess={token => console.log(token)}
-        />
-        <Text>Temporary Login Screen</Text>
-        <Button
-          title="Go on to main screen as if you logged in"
-          onPress={() => this.props.navigation.navigate('MainTab')}
-        />{' '}
+        <Mutation
+          mutation={LOGIN_USER}
+          onCompleted={async data => {
+            const {
+              loginUser: { token, user },
+            } = data
+            if (!data.loginUser.error) {
+              await AsyncStorage.setItem('token', token)
+              await AsyncStorage.setItem('userId', user.id)
+              this.props.navigation.navigate('MainTab')
+            }
+          }}
+        >
+          {(loginUser, { loading, data, error }) => (
+            <View>
+              <FormLabel>Email:</FormLabel>
+              <FormInput
+                onChangeText={newText => this.setState({ email: newText })}
+                type="text"
+                name="email"
+              />
+              <FormLabel>Password:</FormLabel>
+              <FormInput
+                onChangeText={newText => this.setState({ password: newText })}
+                type="password"
+                name="password"
+              />
+
+              {loading && <Message>Logging you in...</Message>}
+              {data &&
+                data.loginUser.error && (
+                  <Message>{data.loginUser.error.message}</Message>
+                )}
+              {error && <Message>Server error</Message>}
+
+              <Button
+                title="Login"
+                disabled={loading}
+                onPress={() => {
+                  const variables = {
+                    email: this.state.email,
+                    password: this.state.password,
+                  }
+                  loginUser({ variables })
+                }}
+              />
+            </View>
+          )}
+        </Mutation>
+        <LinkedInLoginButton />
       </View>
     )
   }
