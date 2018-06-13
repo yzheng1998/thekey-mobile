b5dd8b1c23308b1aa221a1113a8fd0c81dcabaf4
Michele Wang <mwang@Michele-Wangs-MacBook-Pro.local>
2018-06-13 13:12:53 -0400

    Added react-native-linkedin to packages and ios/android builds

--- android/app/src/main/java/com/thekeymobile/MainApplication.java
+++ android/app/src/main/java/com/thekeymobile/MainApplication.java
@@ -3,6 +3,7 @@ package com.thekeymobile;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import net.jodybrewster.linkedinlogin.RNLinkedinLoginPackage;
 import com.microsoft.appcenter.reactnative.crashes.AppCenterReactNativeCrashesPackage;
 import com.microsoft.appcenter.reactnative.analytics.AppCenterReactNativeAnalyticsPackage;
 import com.microsoft.appcenter.reactnative.appcenter.AppCenterReactNativePackage;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNLinkedinLoginPackage(),
             new AppCenterReactNativeCrashesPackage(MainApplication.this, getResources().getString(R.string.appCenterCrashes_whenToSendCrashes)),
             new AppCenterReactNativeAnalyticsPackage(MainApplication.this, getResources().getString(R.string.appCenterAnalytics_whenToEnableAnalytics)),
             new AppCenterReactNativePackage(MainApplication.this),
