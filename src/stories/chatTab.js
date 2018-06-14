6289b926d0ff9053657fcdf471120205006ed00b
Shahd Nara <snara@Shahd-Naras-MacBook-Pro.local>
2018-06-14 13:23:42 -0400

    added story

---
+++ src/stories/chatTab.js
@@ -0,0 +1,18 @@
+import { storiesOf } from '@storybook/react-native'
+import { View } from 'react-native'
+import React from 'react'
+import ChatTab from '../components/ChatTab'
+
+const style = {
+  flex: 1,
+  justifyContent: 'center',
+  alignItems: 'center',
+  backgroundColor: '#F5FCFF',
+}
+const CenteredView = ({ children }) => <View style={style}>{children}</View>
+
+storiesOf('CenteredView').add('default view', () => (
+  <CenteredView>
+    <ChatTab />
+  </CenteredView>
+))
