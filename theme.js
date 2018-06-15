ceb51d278e75a511679efb7bbc1eff31e495d97d
Yuke Zheng <yukezheng@Yukes-MacBook-Pro-2.local>
2018-06-15 13:29:55 -0400

    add themeProvider

---
+++ theme.js
@@ -0,0 +1,25 @@
+const theme = {
+  colors: {
+    background: 'white',
+    buttonPrimary: 'rgb(250, 53, 121)',
+    fontHeader: 'rgb(7, 12, 27)',
+    fontTitle: 'rgb(51, 51, 51)',
+    fontSubtitle: 'rgb(148 157, 170)',
+    fontDescription: 'rgb(176, 18, 200)',
+    fontTag: 'white',
+    fontButtonPrimary: 'white',
+    fontButtonSecondary: 'rgb(100, 108, 132)',
+    fontButtonTertiary: 'rgb(250, 53, 121)',
+    fontProfileFields: 'rgb(78, 158, 255)',
+  },
+  width: {
+    primary: '87.2%',
+  },
+  iosFonts: {
+    bold: 'SFProDisplay-Bold',
+    medium: 'SFProDisplay-Medium',
+    light: 'SFProDisplay-Light',
+  },
+}
+
+export default theme
