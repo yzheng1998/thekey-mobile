6892acb1e9ad078b2e5f217949b69dc0985054b1
Yuke Zheng <yukezheng@Yukes-MacBook-Pro-2.local>
2018-06-15 15:36:15 -0400

    style tag

---
+++ src/screens/SocietyScreen/components/TagLine/components/Tag/styles.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components'
+
+export const TagContainer = styled.View`
+  height: 32;
+  background-color: rgba(0, 0, 0, 0.6);
+  border-radius: 8;
+  overflow: hidden;
+  margin: 5px;
+`
+
+export const TagImage = styled.ImageBackground`
+  width: 100%;
+  height: 100%;
+  opacity: 1;
+  justify-content: center;
+  align-items: center;
+`
+
+export const TagText = styled.Text`
+  font-family: SFProDisplay-Bold;
+  font-size: 14;
+  line-height: 16;
+  color: white;
+  margin-horizontal: 9;
+`
