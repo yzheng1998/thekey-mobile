7b26da0aa71a381e8c50fb0f20d922f5e22fb352
Michele Wang <mwang@Michele-Wangs-MacBook-Pro.local>
2018-06-18 10:28:55 -0400

    Updated Discover Screen styling to match specs

--- src/screens/DiscoverScreen/styles.js
+++ src/screens/DiscoverScreen/styles.js
@@ -1,10 +1,17 @@
 import styled from 'styled-components'
 
-export const Background = styled.ScrollView.attrs({
+export const Background = styled.View`
+  padding-right: 20px;
+  padding-left: 20px;
+  flex: 1;
+`
+export const List = styled.ScrollView.attrs({
   contentContainerStyle: {
     justifyContent: 'center',
     alignItems: 'center',
   },
 })`
   flex: 1;
+  width: 100%;
+  align-self: center;
 `
