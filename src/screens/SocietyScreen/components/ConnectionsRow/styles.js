108bda349e4acce3e3138cb00cf119dec8a26368
Michele Wang <mwang@Michele-Wangs-MacBook-Pro.local>
2018-06-20 16:35:51 -0400

    Updated ConnectionsRow styles to include styling for customizable text prop and use ThemeProvider

--- src/screens/SocietyScreen/components/ConnectionsRow/styles.js
+++ src/screens/SocietyScreen/components/ConnectionsRow/styles.js
@@ -1,23 +1,27 @@
 import styled from 'styled-components'
 import { themeGet } from 'styled-system'
 
+const societyCardTextColor = themeGet('colors.fontSubtitle.primary')
+const eventCardTextColor = themeGet('colors.fontPrimary')
+
 export const ConnectionsRowContainer = styled.View`
   flex-direction: row;
   margin-left: 16;
   margin-bottom: 19;
 `
-
 export const MutualConnections = styled.Text`
   font-size: 16;
   line-height: 19;
   font-family: ${themeGet('fonts.medium')};
-  color: rgb(148, 157, 170);
+  color: ${props =>
+    props.customLabel === 'mutualConnections'
+      ? societyCardTextColor
+      : eventCardTextColor};
   margin-left: 8;
 `
 export const AvatarContainer = styled.View`
   flex-direction: row;
 `
-
 export const Avatar = styled.Image`
   height: ${props => (props.avatarSize ? props.avatarSize : 24)};
   width: ${props => (props.avatarSize ? props.avatarSize : 24)};
