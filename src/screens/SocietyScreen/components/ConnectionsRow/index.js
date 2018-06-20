46008c6b2b65c2634dd0c61d9ed339643a0a3d2d
Yuke Zheng <yukezheng@Yukes-MacBook-Pro-2.local>
2018-06-20 13:15:05 -0400

    make connections row revisions

--- src/screens/SocietyScreen/components/ConnectionsRow/index.js
+++ src/screens/SocietyScreen/components/ConnectionsRow/index.js
@@ -11,19 +11,16 @@ export default class ConnectionsRow extends Component {
     return (
       <ConnectionsRowContainer>
         <AvatarContainer>
-          {this.props.mutualFriends.map(
-            (friend, i) =>
-              i < this.props.avatarNum && (
-                <Avatar
-                  avatarSize={this.props.avatarSize}
-                  source={friend.profilePicture}
-                  key={friend.id}
-                />
-              ),
-          )}
+          {this.props.mutualFriends.map(friend => (
+            <Avatar
+              avatarSize={this.props.avatarSize}
+              source={friend.profilePicture}
+              key={friend.id}
+            />
+          ))}
         </AvatarContainer>
         <MutualConnections>
-          {this.props.mutualFriends.length} mutual connections
+          {`${this.props.connectionsNum} mutual connections`}
         </MutualConnections>
       </ConnectionsRowContainer>
     )
