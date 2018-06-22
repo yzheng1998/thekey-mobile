63787083d90ad7d8f7592702939721538afa4512
Yuke Zheng <yukezheng@Yukes-MacBook-Pro-2.local>
2018-06-22 15:38:07 -0400

    redo ProfilePicBlock using PictureHeader

--- src/screens/SocietyScreen/components/ProfilePicBlock/index.js
+++ src/screens/SocietyScreen/components/ProfilePicBlock/index.js
@@ -1,25 +1,17 @@
 import React, { Component } from 'react'
-import {
-  BlockContainer,
-  BackgroundProfilePic,
-  ProfilePic,
-  EventTitleText,
-  LocationText,
-} from './styles'
+import { EventTitleText, LocationText } from './styles'
+import PictureHeader from '../../../../components/PictureHeader'
 
 export default class ProfilePicBlock extends Component {
   render() {
     const { profilePic, name, city, state } = this.props
     return (
-      <BlockContainer>
-        <BackgroundProfilePic source={profilePic} blurRadius={20}>
-          <ProfilePic source={profilePic} />
-          <EventTitleText>{name}</EventTitleText>
-          <LocationText>
-            {city}, {state}
-          </LocationText>
-        </BackgroundProfilePic>
-      </BlockContainer>
+      <PictureHeader picture={profilePic} size={171}>
+        <EventTitleText>{name}</EventTitleText>
+        <LocationText>
+          {city}, {state}
+        </LocationText>
+      </PictureHeader>
     )
   }
 }
--- src/screens/SocietyScreen/components/ProfilePicBlock/styles.js
+++ src/screens/SocietyScreen/components/ProfilePicBlock/styles.js
@@ -1,25 +1,6 @@
 import styled from 'styled-components'
 import { themeGet } from 'styled-system'
 
-export const BlockContainer = styled.View`
-  overflow: hidden;
-  width: 100%;
-  aspect-ratio: 1.048;
-`
-
-export const BackgroundProfilePic = styled.ImageBackground`
-  width: 100%;
-  height: 100%;
-  align-items: center;
-  justify-content: center;
-`
-
-export const ProfilePic = styled.Image`
-  width: 171;
-  border-radius: ${171 / 2};
-  aspect-ratio: 1;
-`
-
 export const EventTitleText = styled.Text`
   font-size: 24;
   line-height: 29;
