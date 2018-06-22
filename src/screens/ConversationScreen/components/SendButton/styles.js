f5f40a0b37f6808a42c2b297471a6aaba46db107
Michele Wang <mwang@Michele-Wangs-MacBook-Pro.local>
2018-06-22 08:59:08 -0400

    Moved SendButton to conversation screen

---
+++ src/screens/ConversationScreen/components/SendButton/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+import Icon from 'react-native-vector-icons/SimpleLineIcons'
+
+export const ButtonContainer = styled.TouchableOpacity`
+  padding-left: 7px;
+`
+export const SendIcon = styled(Icon)`
+  color: rgb(250, 53, 121);
+  padding-top: 5px;
+`
