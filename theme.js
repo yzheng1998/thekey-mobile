31dee66d9b834dbde37eb31db66c0221838c1ebf
Michele Wang <mwang@Michele-Wangs-MacBook-Pro.local>
2018-06-27 13:38:36 -0400

    Made changes to theme provider tofix colors

--- theme.js
+++ theme.js
@@ -16,7 +16,7 @@ const theme = {
     fontDescription: {
       primary: 'rgb(176, 186, 200)',
       secondary: 'rgb(128, 128, 128)',
-      tertiary: 'rgb(51, 51, 51)',
+      tertiary: 'rgb(69, 77, 88)',
     },
     border: 'rgb(230, 230, 230)',
     fontTag: 'white',
@@ -25,6 +25,12 @@ const theme = {
     fontButtonSecondary: 'rgb(100, 108, 132)',
     fontProfileFields: 'rgb(78, 158, 255)',
     chatCardBorder: 'rgba(142, 142, 147, 0.12)',
+    textAreaBorder: 'rgb(211, 216, 223)',
+    textArea: 'rgb(249, 249, 249)',
+    messageBubbleCurrentUser: 'rgb(250, 53, 121)',
+    messageBubbleOtherUser: 'rgb(75, 67, 91)',
+    like: 'rgba(119, 210, 103, 0.8)',
+    dislike: 'rgba(221, 91, 91, 0.8)',
   },
   width: {
     primary: 0.872 * width,
