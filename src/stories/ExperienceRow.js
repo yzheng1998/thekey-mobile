03d261b75d7a821e299f1632f1dda5fc279e0c72
Yuke Zheng <yukezheng@Yukes-MacBook-Pro-2.local>
2018-06-27 11:29:54 -0400

    make variable name changes to Experience and Education Rows

--- src/stories/ExperienceRow.js
+++ src/stories/ExperienceRow.js
@@ -1,7 +1,7 @@
 import { storiesOf } from '@storybook/react-native'
 import { View } from 'react-native'
 import React from 'react'
-import ExperienceRow from '../screens/ProfileScreen/components/ExperienceRow'
+import ExperienceRow from '../screens/ProfileScreen/components/ExperienceListView/ExperienceRow'
 
 const style = {
   flex: 1,
@@ -14,14 +14,10 @@ const CenteredView = ({ children }) => <View style={style}>{children}</View>
 storiesOf('CenteredView').add('ExperienceRow1', () => (
   <CenteredView>
     <ExperienceRow
-      company="Unfiltered Network"
+      companyName="Unfiltered Network"
       position="CEO"
-      yearStart="2016"
-      yearEnd="Present / 1yr 9 mos"
-      badge={{
-        uri:
-          'https://logoeps.com/wp-content/uploads/2012/07/harvard-university-logo-vector-01.png',
-      }}
+      startYear="2016"
+      endYear="Present / 1yr 9 mos"
     />
   </CenteredView>
 ))
