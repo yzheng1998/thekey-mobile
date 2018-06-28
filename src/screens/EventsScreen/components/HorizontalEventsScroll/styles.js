8e706c4e74df2cca8dcda46ccc2c089539d6cbd5
Michele Wang <mwang@Michele-Wangs-MacBook-Pro.local>
2018-06-28 15:38:58 -0400

    Added Horizontal event scroll component showcasing LargeEventCard

---
+++ src/screens/EventsScreen/components/HorizontalEventsScroll/styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const Container = styled.View`
+  width: 100%;
+  align-items: center;
+`
+export const CardContainer = styled.View`
+  box-shadow: 1px 16px 8px rgb(169,169,169)};
+  margin: 15px;
+  padding: 3px;
+  padding-bottom: 20px;
+`
+export const CardList = styled.FlatList``
