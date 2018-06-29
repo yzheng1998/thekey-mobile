5727fc81e02250cbeb9d8aa5176f908bcd14f98b
Shahd Nara <snara@Shahd-Naras-MacBook-Pro.local>
2018-06-29 13:46:06 -0400

    makes button group functional

--- src/screens/ApplyNowScreen/index.js
+++ src/screens/ApplyNowScreen/index.js
@@ -14,19 +14,30 @@ import TemplateSelector from './components/ButtonGroup'
 import BackButton from 'react-native-vector-icons/Ionicons'
 import KeyboardSpacer from 'react-native-keyboard-spacer'
 
+const CUSTOM = 1
+
+const coverLetter =
+  'This would be a default cover letter that a user could configure in their settings. From here they could send as-is, or adjust the text to better suit the position in question.'
+
 export default class ApplyNowScreen extends Component {
   constructor() {
     super()
     this.state = {
-      selectedIndex: 0,
+      selectedIndex: CUSTOM,
+      content: '',
     }
     this.updateIndex = this.updateIndex
   }
+
   updateIndex = selectedIndex => {
-    this.setState({ selectedIndex })
+    this.setState({
+      selectedIndex,
+      content: selectedIndex === CUSTOM ? '' : coverLetter,
+    })
   }
   render() {
     const { selectedIndex } = this.state
+    console.log(this.state.content)
     return (
       <BigContainer>
         <Container>
@@ -51,7 +62,8 @@ export default class ApplyNowScreen extends Component {
           <LetterInput
             multiline
             autoGrow={false}
-            placeholder="This would be a default cover letter that a user could configure in their settings. From here they could send as-is, or adjust the text to better suit the position in question."
+            value={this.state.content}
+            onChangeText={content => this.setState({ content })}
             placeholderTextColor="rgb(69,77,88)"
           />
         </Container>
