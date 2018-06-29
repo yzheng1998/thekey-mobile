078c5bc9c416bd7afb10ee7947b3f5bab2481a6b
Michele Wang <mwang@Michele-Wangs-MacBook-Pro.local>
2018-06-29 13:37:14 -0400

    Added EventsHeader component containing title and buttons

---
+++ src/screens/EventsScreen/components/EventsHeader/styles.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components'
+import { themeGet } from 'styled-system'
+
+export const ButtonRow = styled.View`
+  width: 100%;
+  padding-left: 20px;
+  padding-right: 20px;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`
+export const HeaderBackground = styled.View`
+  width: 100%;
+  justify-content: flex-start;
+  flex-direction: column;
+  background-color: black;
+  padding-top: 40px;
+  padding-bottom: 10px;
+`
+export const Title = styled.Text`
+  color: ${themeGet('colors.fontPrimary')};
+  font-family: ${themeGet('fonts.heavy')};
+  font-size: 30px;
+  margin-left: 20px;
+  margin-bottom: 15px;
+`
+export const NewEventButton = styled.TouchableOpacity``
+export const BackButton = styled.TouchableOpacity``
