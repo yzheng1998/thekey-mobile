68fa2810760c7d0f0bd442a838f5338b6ab1abe0
Michele Wang <mwang@Michele-Wangs-MacBook-Pro.local>
2018-06-29 13:56:44 -0400

    Added separate header component

---
+++ src/screens/SocietyScreen/components/SocietyHeader/index.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import {
+  HeaderBackground,
+  ButtonRow,
+  BackButton,
+  SearchButton,
+  Title,
+} from './styles'
+import Icon from 'react-native-vector-icons/Ionicons'
+
+export default class EventsHeader extends Component {
+  render() {
+    return (
+      <HeaderBackground>
+        <ButtonRow>
+          <BackButton onPress={() => this.props.navigation.goBack()}>
+            <Icon name="ios-arrow-back" color="white" size={30} />
+          </BackButton>
+          <SearchButton onPress={() => {}}>
+            <Icon name="md-search" color="white" size={25} />
+          </SearchButton>
+        </ButtonRow>
+        <Title>The Society</Title>
+      </HeaderBackground>
+    )
+  }
+}
