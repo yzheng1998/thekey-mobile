86e5f78ddc25a6d2b3f4ed28d7d152827f3897fc
Michele Wang <mwang@Michele-Wangs-MacBook-Pro.local>
2018-06-29 11:27:42 -0400

    Updated SocietyCard styles to reduce lower margins

--- src/screens/SocietyScreen/components/SocietyCard/styles.js
+++ src/screens/SocietyScreen/components/SocietyCard/styles.js
@@ -3,6 +3,7 @@ import { themeGet } from 'styled-system'
 
 export const CardContainer = styled.View`
   width: ${themeGet('width.primary')};
+  height: 540px;
   border-radius: 20;
   background-color: ${themeGet('colors.background')}
   overflow: hidden;
@@ -12,10 +13,9 @@ export const CardContainer = styled.View`
 export const BioText = styled.Text`
   font-size: 16;
   line-height: 23;
-  height: 69;
+  height: 69px;
   font-family: ${themeGet('fonts.light')};
   color: rgb(69, 77, 88);
-  margin-bottom: 18;
   margin-left: 16;
   margin-right: 16;
 `
