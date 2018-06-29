ae77b1017f4a981e0e62999c131c725527223e70
Michele Wang <mwang@Michele-Wangs-MacBook-Pro.local>
2018-06-29 15:26:45 -0400

    Add back button styled component

--- src/screens/JobsScreen/styles.js
+++ src/screens/JobsScreen/styles.js
@@ -14,8 +14,11 @@ export const HeaderBackground = styled.View`
 `
 export const Title = styled.Text`
   color: ${themeGet('colors.fontPrimary')};
-  font-family: ${themeGet('fonts.bold')};
+  font-family: ${themeGet('fonts.heavy')};
   font-size: 30px;
   margin-left: 20px;
-  margin-bottom: 15px;
+  margin-bottom: 10px;
+`
+export const BackButton = styled.TouchableOpacity`
+  margin-left: 20px;
 `
