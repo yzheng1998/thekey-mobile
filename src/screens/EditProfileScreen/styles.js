2aa8dd362592c6813702e6eb9eba6fdfe30795db
Yuke Zheng <yukezheng@Yukes-MacBook-Pro-2.local>
2018-06-29 09:50:51 -0400

    create EditContactBlock

--- src/screens/EditProfileScreen/styles.js
+++ src/screens/EditProfileScreen/styles.js
@@ -14,9 +14,45 @@ export const Title = styled.Text`
   font-family: ${themeGet('fonts.bold')};
 `
 
+export const BlockTitle = styled.Text`
+  margin-bottom: 8px;
+  font-size: 16px;
+  font-family: ${themeGet('fonts.bold')};
+`
+
 export const Content = styled.TextInput`
   flex: 3;
   font-size: 16px;
   font-family: ${themeGet('fonts.medium')};
   color: ${themeGet('colors.fontProfileFields')};
+`
+
+export const ContactContent = styled.TextInput`
+  font-size: 18px;
+  font-family: ${themeGet('fonts.light')};
+  line-height: 23px;
+  color: ${themeGet('colors.fontProfileFields')};
+  margin-top: 3px;
+  margin-bottom: 3px;
+  margin-left: 5px;
+`
+
+export const ContactContainer = styled.View`
+  margin-bottom: 12px;
+`
+
+export const Contact = styled.View`
+  flex-direction: row;
+  align-items: center;
+  margin-top: 3px;
+  margin-bottom: 3px;
+`
+
+export const Container = styled.View`
+  width: 100%;
+  justify-content: space-between;
+  margin-top: 5;
+  margin-bottom: 5;
+  padding-left: 16;
+  padding-right: 16;
 `
