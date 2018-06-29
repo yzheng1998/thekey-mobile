71053355a0d9f0c57667e5c3ece30f802b87b320
Sam Kaplan <sam@rubinkaplan.com>
2018-06-28 22:02:52 -0400

    Setup apollo client subscriptions

--- App.js
+++ App.js
@@ -1,25 +1,10 @@
 import React, { Component } from 'react'
-import ApolloClient from 'apollo-boost'
-import { ApolloProvider } from 'react-apollo'
-import config from './config'
-import RootNavigator from './src/navigation/RootNavigator'
-import { AsyncStorage } from 'react-native'
-import { ThemeProvider } from 'styled-components/native'
-import theme from './theme'
 
-const client = new ApolloClient({
-  uri: config.graphqlUrl,
-  request: async operation => {
-    const token = await AsyncStorage.getItem('token')
-    if (token) {
-      operation.setContext({
-        headers: {
-          authorization: token,
-        },
-      })
-    }
-  },
-})
+import { ApolloProvider } from 'react-apollo'
+import RootNavigator from './src/navigation/RootNavigator'
+import { ThemeProvider } from 'styled-components/native'
+import { client } from './src/apollo'
+import theme from './theme'
 
 export default class App extends Component {
   render() {
