d1aac89aa6b793f9ceea1fd752f78598cb0952d2
Yuke Zheng <yukezheng@Yukes-MacBook-Pro-2.local>
2018-07-02 15:41:31 -0400

    create edit education block

---
+++ src/screens/EditProfileScreen/components/EditExperienceBlock/index.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import { ExperienceList, WideContainer } from '../../styles'
+import ExperienceRow from '../../../ProfileScreen/components/ExperienceListView/ExperienceRow'
+import Suitcase from 'react-native-vector-icons/Entypo'
+import BlockButton from '../../../../components/BlockButton'
+
+export default class EditExperienceBlock extends Component {
+  render() {
+    return (
+      <WideContainer>
+        <ExperienceList>
+          <Suitcase name="suitcase" size={36} style={{ marginBottom: 4 }} />
+          {this.props.experienceData.map(experience => (
+            <ExperienceRow
+              companyName={experience.companyName}
+              position={experience.position}
+              startYear={experience.startYear}
+              endYear={experience.endYear}
+              key={experience.id}
+            />
+          ))}
+        </ExperienceList>
+        <BlockButton text="ADD EXPERIENCE" />
+      </WideContainer>
+    )
+  }
+}
