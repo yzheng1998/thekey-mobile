81e902027a0ce254167622f3bfac4728ea9e5591
Michele Wang <mwang@Michele-Wangs-MacBook-Pro.local>
2018-07-02 14:10:25 -0400

    Added component to generate QR code

---
+++ src/components/QRCodeGenerator/index.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import gql from 'graphql-tag'
+import { Query } from 'react-apollo'
+import QRCode from 'react-native-qrcode'
+
+const GET_USERID = gql`
+  query viewer {
+    viewer {
+      id
+    }
+  }
+`
+export default class QRCodeGenerator extends Component {
+  render() {
+    return (
+      <Query query={GET_USERID}>
+        {({ loading, error, data }) => {
+          if (loading) return 'Loading...'
+          if (error) return `Error! ${error.message}`
+          return (
+            <QRCode
+              value={data.viewer.id}
+              size={200}
+              bgColor="black"
+              fgColor="white"
+            />
+          )
+        }}
+      </Query>
+    )
+  }
+}
