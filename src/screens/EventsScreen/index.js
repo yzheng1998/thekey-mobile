e1e4ef4fca01bd0a88bbf44c2bcc61ec5e4a0b23
Yuke Zheng <yukezheng@Yukes-MacBook-Pro-2.local>
2018-07-12 09:43:03 -0400

    fix lint

--- src/screens/EventsScreen/index.js
+++ src/screens/EventsScreen/index.js
@@ -69,48 +69,8 @@ class EventsScreen extends Component {
       }
     }
 
-    // hardcoding array of friends for now
-    const friends = [
-      {
-        firstName: 'Yuke',
-        id: 1,
-        profilePicture: {
-          uri:
-            'https://scontent.fzty2-1.fna.fbcdn.net/v/t31.0-8/19095354_1322253334562342_5268478069300274794_o.jpg?_nc_cat=0&oh=5998f02ad58ac913850952492aaa62ba&oe=5BBDE33A',
-        },
-      },
-      {
-        firstName: 'Noah',
-        id: 2,
-        profilePicture: {
-          uri: 'https://www.dev.hsa.net/img/team/Noah.jpg',
-        },
-      },
-      {
-        firstName: 'Humprey',
-        id: 3,
-        profilePicture: {
-          uri: 'https://www.dev.hsa.net/img/team/humphrey.JPG',
-        },
-      },
-      {
-        firstName: 'Ivraj',
-        id: 4,
-        profilePicture: {
-          uri: 'https://www.dev.hsa.net/img/team/Ivraj.jpg',
-        },
-      },
-      {
-        firstName: 'Jovi',
-        id: 5,
-        profilePicture: {
-          uri: 'https://www.dev.hsa.net/img/team/Jovin.jpg',
-        },
-      },
-    ]
-    
     const { searchText } = this.state
-    
+
     return (
       <View>
         <EventsHeader
