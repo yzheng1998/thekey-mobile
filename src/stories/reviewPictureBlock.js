adab1bdefaf77748ba00f6aa07850c77beaded86
Michele Wang <mwang@Michele-Wangs-MacBook-Pro.local>
2018-07-12 11:37:12 -0400

    Updated component stories' paths

--- src/stories/reviewPictureBlock.js
+++ src/stories/reviewPictureBlock.js
@@ -1,7 +1,7 @@
 import { storiesOf } from '@storybook/react-native'
 import React from 'react'
 import { View } from 'react-native'
-import ReviewPictureBlock from '../components/ReviewPictureBlock'
+import ReviewPictureBlock from '../screens/ReviewScreen/components/ReviewPictureBlock'
 
 const style = {
   flex: 1,
