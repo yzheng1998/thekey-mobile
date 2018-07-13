6a0660dbc3ce60a4c2e808610798c7800e71415f
Michele Wang <mwang@Michele-Wangs-MacBook-Pro.local>
2018-07-13 14:34:43 -0400

    Updated Network Screen styling

--- src/screens/NetworkScreen/styles.js
+++ src/screens/NetworkScreen/styles.js
@@ -14,7 +14,7 @@ export const HeaderBackground = styled.View`
 `
 export const Title = styled.Text`
   color: ${themeGet('colors.fontPrimary')};
-  font-family: ${themeGet('fonts.bold')};
+  font-family: ${themeGet('fonts.heavy')};
   font-size: 30px;
   margin-left: 20px;
   margin-bottom: 15px;
