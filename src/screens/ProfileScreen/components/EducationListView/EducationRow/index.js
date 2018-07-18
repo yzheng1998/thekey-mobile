c88b597c9ff4f567d92415ceddac8848a43c1840
Michele Wang <mwang@Michele-Wangs-MacBook-Pro.local>
2018-07-18 09:57:14 -0400

    pass in navigation props to AddEducation form from EducationRow

--- src/screens/ProfileScreen/components/EducationListView/EducationRow/index.js
+++ src/screens/ProfileScreen/components/EducationListView/EducationRow/index.js
@@ -4,10 +4,6 @@ import ProfileInfoRow from '../../../../../components/ProfileInfoRow'
 export default class EducationRow extends Component {
   render() {
     const { schoolName, degreeType, major, startYear, endYear } = this.props
-<<<<<<< HEAD
-=======
-    console.log('props', this.props)
->>>>>>> 7a0983a... fix up prop naming issues in Profile Screen
     return (
       <ProfileInfoRow
         education
