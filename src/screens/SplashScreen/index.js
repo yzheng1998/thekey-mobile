d6cd0a3cf0245ba24e76c01920247fa5a171b414
Michele Wang <mwang@Michele-Wangs-MacBook-Pro.local>
2018-07-18 16:42:29 -0400

    updated Splash Screen and added LinkedIn Login Button component

--- src/screens/SplashScreen/index.js
+++ src/screens/SplashScreen/index.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react'
-import { View, Image } from 'react-native'
-import { Container, Content, SubTitle, Title } from './styles'
+import { Image } from 'react-native'
+import {
+  Background,
+  Container,
+  Content,
+  SubTitle,
+  Title,
+  SignInButton,
+  SignInText,
+  RegisterButton,
+  RegisterText,
+  DividerRow,
+  Divider,
+  DividerText,
+} from './styles'
 import Logo from '../../stories/thekey-logo.png'
+import LinkedInLoginButton from './components/LinkedInLoginButton'
 
-export class SplashScreen extends Component {
+export default class SplashScreen extends Component {
   render() {
     return (
-      <View>
+      <Background>
         <Container>
           <Content>
             <Image source={Logo} style={{ height: 105, width: 79 }} />
@@ -14,7 +28,23 @@ export class SplashScreen extends Component {
             <Title>The Key</Title>
           </Content>
         </Container>
-      </View>
+        <Container>
+          <SignInButton onPress={() => this.props.navigation.navigate('Login')}>
+            <SignInText>SIGN IN</SignInText>
+          </SignInButton>
+          <RegisterButton
+            onPress={() => this.props.navigation.navigate('Register')}
+          >
+            <RegisterText>REGISTER</RegisterText>
+          </RegisterButton>
+          <DividerRow>
+            <Divider />
+            <DividerText>OR</DividerText>
+            <Divider />
+          </DividerRow>
+          <LinkedInLoginButton />
+        </Container>
+      </Background>
     )
   }
 }
--- src/screens/SplashScreen/styles.js
+++ src/screens/SplashScreen/styles.js
@@ -1,13 +1,18 @@
 import styled from 'styled-components'
 import { themeGet } from 'styled-system'
 
-export const Container = styled.View`
-  height: 303px;
+export const Background = styled.ScrollView`
+  flex: 1;
   width: 100%;
   background-color: white;
-  border-width: 1px;
+`
+export const Container = styled.View`
+  width: 100%;
+  height: 325px;
+  background-color: white;
   border-color: ${themeGet('colors.border')};
   align-items: center;
+  padding: 24px;
 `
 export const Content = styled.View`
   padding-top: 79px;
@@ -16,12 +21,12 @@ export const Content = styled.View`
 export const SubTitle = styled.Text`
   font-family: ${themeGet('fonts.bold')};
   color: ${themeGet('colors.fontSubtitle.primary')};
-  font-size: 14px;
+  font-size: 18px;
 `
 export const Title = styled.Text`
   font-family: ${themeGet('fonts.heavy')};
   color: black;
-  font-size: 40px;
+  font-size: 48px;
 `
 export const SignInText = styled.Text`
   font-family: ${themeGet('fonts.heavy')};
@@ -32,6 +37,24 @@ export const RegisterText = styled.Text`
   font-family: ${themeGet('fonts.heavy')};
   color: ${themeGet('colors.buttonPrimary')};
   font-size: 14px;
+`
+export const DividerText = styled.Text`
+  font-family: ${themeGet('fonts.bold')};
+  color: ${themeGet('colors.fontDescription.primary')};
+  font-size: 12px;
+`
+export const Divider = styled.View`
+  width: 47%;
+  background-color: ${themeGet('colors.fontDescription.primary')};
+  height: 1px;
+  margin-left: 3px;
+  margin-right: 3px;
+`
+export const DividerRow = styled.View`
+  flex-direction: row;
+  margin-top: 15px;
+  margin-bottom: 15px;
+  align-items: center;
 `
 export const SignInButton = styled.TouchableOpacity`
   height: 50px;
@@ -44,12 +67,12 @@ export const SignInButton = styled.TouchableOpacity`
   margin-left: 12px;
   margin-right: 12px;
 `
-export const RemoveButton = styled.TouchableOpacity`
+export const RegisterButton = styled.TouchableOpacity`
   width: 100%;
   border-color: ${themeGet('colors.buttonPrimary')};
-  border-width: 5px;
+  border-width: 2px;
   background-color: white
-  margin-top: 24px;
+  margin-top: 15px;
   border-radius: 10px;
   align-items: center;
   padding: 16px;
