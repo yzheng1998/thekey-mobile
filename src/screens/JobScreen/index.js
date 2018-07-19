b17daa17de2fda0834e552f2f5994a32d0020ee2
Michele Wang <mwang@Michele-Wangs-MacBook-Pro.local>
2018-07-19 14:36:43 -0400

    Added to Job query and pass job summary info as props to components

--- src/screens/JobScreen/index.js
+++ src/screens/JobScreen/index.js
@@ -30,6 +30,10 @@ const GET_JOB = gql`
       tags {
         name
       }
+      aboutRole
+      aboutCompany
+      bringToRole
+      industry
     }
   }
 `
@@ -59,6 +63,10 @@ class JobScreen extends Component {
             commitment,
             deadline,
             tags,
+            aboutRole,
+            aboutCompany,
+            bringToRole,
+            industry,
           } = data.job
           return (
             <Container>
@@ -78,6 +86,11 @@ class JobScreen extends Component {
               <AboutBlock
                 navigation={this.props.navigation}
                 about={description}
+                aboutRole={aboutRole}
+                aboutCompany={aboutCompany}
+                bringToRole={bringToRole}
+                industry={industry}
+                commitment={commitment}
               />
               <TagsContainer>
                 <TagLine tagData={tags} lines={1} />
