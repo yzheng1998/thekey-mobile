6ba4ec60275658fef70d078836c72908d4ccb7e4
Yuke Zheng <yukezheng@Yukes-MacBook-Pro-2.local>
2018-07-19 09:39:36 -0400

    fix styles on registration progress bar

--- src/components/RegistrationProgressBar/styles.js
+++ src/components/RegistrationProgressBar/styles.js
@@ -4,11 +4,13 @@ import { themeGet } from 'styled-system'
 const progressBarHeight = '4px'
 
 export const EmptyBar = styled.View`
-  width: 90%;
+  margin-left: 12px;
+  margin-right: 12px;
   background-color: ${themeGet('colors.progressBarEmpty')};
   height: ${progressBarHeight};
   border-radius: 8px;
   overflow: hidden;
+  margin-bottom: 12px;
 `
 export const FilledBar = styled.View`
   width: ${props => props.progress};
