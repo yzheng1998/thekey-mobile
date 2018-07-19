5de84948f6d92bafff693eebc4c1116159b16d66
Yuke Zheng <yukezheng@Yukes-MacBook-Pro-2.local>
2018-07-19 16:44:32 -0400

    add icon to school search item

--- src/components/SchoolSearchCard/styles.js
+++ src/components/SchoolSearchCard/styles.js
@@ -1,10 +1,12 @@
 import styled from 'styled-components'
 import { themeGet } from 'styled-system'
 
-export const Container = styled.SafeAreaView`
+export const Container = styled.TouchableOpacity`
   width: 100%;
   height: 68px;
-  justify-content: center;
+  flex-direction: row;
+  align-items: center;
+  padding-left: 16px;
 `
 export const SchoolName = styled.Text`
   font-family: ${themeGet('fonts.bold')};
@@ -15,7 +17,7 @@ export const SchoolName = styled.Text`
 
 export const Location = styled.Text`
   font-family: ${themeGet('fonts.semiBold')};
-  color: ${themeGet('colors.fontDescription.quaternary')}
+  color: ${themeGet('colors.fontDescription.quaternary')};
   font-size: 12px;
   letter-spacing: -0.1;
   margin-left: 16px;
