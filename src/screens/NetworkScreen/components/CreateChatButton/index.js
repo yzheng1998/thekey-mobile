231bcbbdcbcc2d70d33e499dd0fce9e517c5e7f8
Michele Wang <mwang@Michele-Wangs-MacBook-Pro.local>
2018-07-20 01:15:55 -0400

    Added createChat mutation and navigate to Conversation screen once mutation is complete

--- src/screens/NetworkScreen/components/CreateChatButton/index.js
+++ src/screens/NetworkScreen/components/CreateChatButton/index.js
@@ -1,13 +1,45 @@
 import React, { Component } from 'react'
 import { Button, Text } from './styles'
+import { Mutation } from 'react-apollo'
+import gql from 'graphql-tag'
+
+const CREATE_CHAT = gql`
+  mutation createChat($participantIds: [ID!]!) {
+    createChat(participantIds: $participantIds) {
+      chat {
+        id
+        participants {
+          firstName
+          id
+        }
+      }
+    }
+  }
+`
 
 export default class CreateChatButton extends Component {
   render() {
-    const { onPress } = this.props
+    const { participantIds, createNewChat } = this.props
     return (
-      <Button onPress={onPress}>
-        <Text>Next</Text>
-      </Button>
+      <Mutation
+        mutation={CREATE_CHAT}
+        onCompleted={data => {
+          createNewChat(data.createChat.chat)
+        }}
+      >
+        {createChat => (
+          <Button
+            onPress={() => {
+              const variables = {
+                participantIds,
+              }
+              createChat({ variables })
+            }}
+          >
+            <Text>Next</Text>
+          </Button>
+        )}
+      </Mutation>
     )
   }
 }
