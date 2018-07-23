64206f1e7b20527d4617be5535b0f13abcb724e0
Yuke Zheng <yukezheng@Yukes-MacBook-Pro-2.local>
2018-07-23 13:47:38 -0400

    fix header, commitment options

--- src/screens/SimilarJobsScreen/styles.js
+++ src/screens/SimilarJobsScreen/styles.js
@@ -8,6 +8,7 @@ export const HeaderBackground = styled.View`
   width: 100%;
   height: 88px;
   flex-direction: row;
+  align-items: center;
   justify-content: center;
   background-color: black;
 `
@@ -15,8 +16,7 @@ export const Title = styled.Text`
   color: white;
   font-family: ${themeGet('fonts.heavy')};
   font-size: 20px;
-  margin-left: 20px;
-  top: 45px;
+  margin-top: 30px;
 `
 export const BackButtonContainer = styled.TouchableOpacity`
   position: absolute;
