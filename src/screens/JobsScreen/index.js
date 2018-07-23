1a79a16335d893fed35d05918fb88c8a8b99321d
Michele Wang <mwang@Michele-Wangs-MacBook-Pro.local>
2018-07-23 15:16:18 -0400

    Added updated Jobs screen header with SafeView

--- src/screens/JobsScreen/index.js
+++ src/screens/JobsScreen/index.js
@@ -3,16 +3,9 @@ import gql from 'graphql-tag'
 import { View, FlatList, ScrollView, Text } from 'react-native'
 import SearchBar from '../../components/SearchBar'
 import JobCard from '../../components/JobCard'
-import SearchFilterTab from '../../components/SearchFilterTab'
-import {
-  HeaderBackground,
-  Title,
-  BackButton,
-  Divider,
-  CardDivider,
-} from './styles'
+import JobsHeader from './components/JobsHeader'
+import { Divider, CardDivider } from './styles'
 import { Query } from 'react-apollo'
-import BackArrow from 'react-native-vector-icons/Ionicons'
 
 const GET_JOBS = gql`
   query jobs($jobsFilterInput: JobsFilterInput!) {
@@ -53,52 +46,46 @@ class JobsScreen extends Component {
       },
     }
     return (
-      <ScrollView keyboardShouldPersistTaps="always">
-        <HeaderBackground>
-          <BackButton onPress={() => this.props.navigation.goBack()}>
-            <BackArrow name="ios-arrow-back" color="white" size={30} />
-          </BackButton>
-          <Title>Jobs/Internships</Title>
-          <SearchFilterTab
+      <View style={{ flex: 1 }}>
+        <ScrollView keyboardShouldPersistTaps="always">
+          <JobsHeader
+            navigation={this.props.navigation}
+            changeTab={this.changeTab}
             selectedIndex={this.state.tab}
-            updateState={this.changeTab}
-            color="white"
-            selectedColor="white"
-            options={['All', 'Saved', 'Applied For']}
           />
-        </HeaderBackground>
-        <SearchBar
-          updateText={this.updateText}
-          searchText={searchText}
-          placeholderText="Search Jobs & Internships"
-        />
-        <Divider />
-        <Query query={GET_JOBS} variables={variables}>
-          {({ loading, error, data }) => {
-            if (loading) return <Text>Loading...</Text>
-            if (error) {
-              return <Text>Error! {error.message}</Text>
-            }
-            return (
-              <FlatList
-                keyExtractor={job => job.id}
-                data={data.jobs}
-                renderItem={({ item: job }) => (
-                  <View>
-                    <JobCard
-                      navigate={id =>
-                        this.props.navigation.navigate('Job', { id })
-                      }
-                      job={job}
-                    />
-                    <CardDivider />
-                  </View>
-                )}
-              />
-            )
-          }}
-        </Query>
-      </ScrollView>
+          <SearchBar
+            updateText={this.updateText}
+            searchText={searchText}
+            placeholderText="Search Jobs & Internships"
+          />
+          <Divider />
+          <Query query={GET_JOBS} variables={variables}>
+            {({ loading, error, data }) => {
+              if (loading) return <Text>Loading...</Text>
+              if (error) {
+                return <Text>Error! {error.message}</Text>
+              }
+              return (
+                <FlatList
+                  keyExtractor={job => job.id}
+                  data={data.jobs}
+                  renderItem={({ item: job }) => (
+                    <View>
+                      <JobCard
+                        navigate={id =>
+                          this.props.navigation.navigate('Job', { id })
+                        }
+                        job={job}
+                      />
+                      <CardDivider />
+                    </View>
+                  )}
+                />
+              )
+            }}
+          </Query>
+        </ScrollView>
+      </View>
     )
   }
 }
--- src/screens/JobsScreen/styles.js
+++ src/screens/JobsScreen/styles.js
@@ -4,31 +4,11 @@ import { themeGet } from 'styled-system'
 export const Background = styled.View`
   flex: 1;
 `
-export const HeaderBackground = styled.View`
-  width: 100%;
-  justify-content: flex-start;
-  flex-direction: column;
-  background-color: black;
-  padding-top: 40px;
-  padding-bottom: 10px;
-`
-export const Title = styled.Text`
-  color: ${themeGet('colors.fontPrimary')};
-  font-family: ${themeGet('fonts.heavy')};
-  font-size: 30px;
-  margin-left: 20px;
-  margin-bottom: 10px;
-`
-export const BackButton = styled.TouchableOpacity`
-  margin-left: 20px;
-`
-
 export const Divider = styled.View`
   width: 100%;
   height: 8px;
   background-color: ${themeGet('colors.divider')};
 `
-
 export const CardDivider = styled.View`
   height: 2px;
   background-color: ${themeGet('colors.divider')};
