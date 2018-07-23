3c4bca46cd5dfc8efbdc47a0996300d528928511
Michele Wang <mwang@Michele-Wangs-MacBook-Pro.local>
2018-07-23 16:29:01 -0400

    Update ProfileScreen styling

--- src/screens/ProfileScreen/styles.js
+++ src/screens/ProfileScreen/styles.js
@@ -5,7 +5,6 @@ export const ScreenContainer = styled.ScrollView`
   width: 100%;
   background-color: ${themeGet('colors.background')};
 `
-
 export const Divider = styled.View`
   width: 100%;
   height: 4px;
@@ -13,7 +12,6 @@ export const Divider = styled.View`
   margin-bottom: 10px;
   background-color: ${themeGet('colors.divider')};
 `
-
 export const Container = styled.View`
   width: 100%;
   flex-direction: ${props => (props.row ? 'row' : 'column')};
@@ -23,7 +21,6 @@ export const Container = styled.View`
   padding-left: 16;
   padding-right: 16;
 `
-
 export const Title = styled.Text`
   font-size: 16px;
   font-family: ${themeGet('fonts.bold')};
