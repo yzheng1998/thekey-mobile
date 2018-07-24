069f0b3ef70c11e16f11e7bccd742ea83f2249ff
Michele Wang <mwang@Michele-Wangs-MacBook-Pro.local>
2018-07-24 18:04:32 -0400

    updated ConversationScreen queries

--- src/screens/ConversationScreen/queries.js
+++ src/screens/ConversationScreen/queries.js
@@ -4,6 +4,9 @@ export const GET_CHAT_AND_VIEWER = gql`
   query chatAndViewer($chatId: ID!) {
     viewer {
       id
+      firstName
+      lastName
+      profilePicture
     }
     chat(id: $chatId) {
       participants {
