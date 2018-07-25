7c4da31e76b2cd96ef799881f1200650ab6d9c17
Michele Wang <mwang@Michele-Wangs-MacBook-Pro.local>
2018-07-25 16:54:46 -0400

    Update text for when you have 1, 0, or more than 1 mutual connection

--- src/screens/SocietyScreen/components/MutualConnectionsRow/index.js
+++ src/screens/SocietyScreen/components/MutualConnectionsRow/index.js
@@ -6,6 +6,19 @@ import {
   AvatarContainer,
 } from './styles'
 
+const renderConnectionsText = connectionsNum => {
+  if (connectionsNum === 0) {
+    return '0 mutual connections'
+  } else if (connectionsNum === 1) {
+    return '1 mutual connection'
+  }
+  return `${connectionsNum} mutual connections`
+}
+
+const ConnectionsText = ({ connectionsNum }) => (
+  <MutualConnections>{renderConnectionsText(connectionsNum)}</MutualConnections>
+)
+
 export default class MutualConnectionsRow extends Component {
   render() {
     const { connectionsNum, mutualFriends, avatarSize } = this.props
@@ -27,11 +40,7 @@ export default class MutualConnectionsRow extends Component {
             />
           ))}
         </AvatarContainer>
-        <MutualConnections>
-          {connectionsNum
-            ? `${connectionsNum} mutual connections`
-            : `0 mutual connections`}
-        </MutualConnections>
+        <ConnectionsText connectionsNum={connectionsNum} />
       </ConnectionsRowContainer>
     )
   }
