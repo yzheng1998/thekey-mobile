a43d3b12d894dd0ab26d277cb61ee14e79b4ee04
Michele Wang <mwang@Michele-Wangs-MacBook-Pro.local>
2018-07-27 15:08:06 -0400

    Styled InterestCard by adding borders and plus icons to the card

--- src/screens/EditProfileScreen/components/InterestsSearchModal/components/InterestCard/index.js
+++ src/screens/EditProfileScreen/components/InterestsSearchModal/components/InterestCard/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Card, Title } from './styles'
 import { TouchableOpacity } from 'react-native'
+import PlusIcon from 'react-native-vector-icons/Feather'
 
 export default class InterestCard extends Component {
   render() {
@@ -9,6 +10,7 @@ export default class InterestCard extends Component {
       <TouchableOpacity onPress={onPress}>
         <Card>
           <Title>{title}</Title>
+          <PlusIcon name="plus-circle" color="rgb(250, 53, 121)" size={20} />
         </Card>
       </TouchableOpacity>
     )
--- src/screens/EditProfileScreen/components/InterestsSearchModal/components/InterestCard/styles.js
+++ src/screens/EditProfileScreen/components/InterestsSearchModal/components/InterestCard/styles.js
@@ -2,13 +2,16 @@ import styled from 'styled-components'
 import { themeGet } from 'styled-system'
 
 export const Card = styled.View`
-  height: 30px;
+  height: 40px;
   width: 100%;
   background-color: white;
   flex-direction: row;
-  justify-content: flex-start;
+  justify-content: space-between;
   align-items: center;
   margin-left: 5px;
+  padding-right: 15px;
+  border-bottom-width: 1px;
+  border-bottom-color: ${themeGet('colors.divider')};
 `
 export const Title = styled.Text`
   font-size: 16px;
