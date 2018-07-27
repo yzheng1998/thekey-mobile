d4544a87ee2e6c5a192620758130f359d5c0f7e1
Michele Wang <mwang@Michele-Wangs-MacBook-Pro.local>
2018-07-27 13:25:31 -0400

    added AddCompanyReview mutation

--- src/screens/AddCompanyReviewScreen/components/SubmitReviewButton/index.js
+++ src/screens/AddCompanyReviewScreen/components/SubmitReviewButton/index.js
@@ -1,6 +1,33 @@
 import React, { Component } from 'react'
 import { SubmitButton, SubmitButtonText } from './styles'
+import gql from 'graphql-tag'
+import { Mutation } from 'react-apollo'
 
+const ADD_COMPANY_REVIEW = gql`
+  mutation addCompanyReview($addCompanyReviewInput: AddCompanyReviewInput!) {
+    addCompanyReview(addCompanyReviewInput: $addCompanyReviewInput) {
+      companyReview {
+        id
+        rating
+        title
+        pros
+        cons
+        employmentType
+        current
+        jobTitle
+        location
+        company {
+          id
+        }
+        lastWorked
+        createdAt
+      }
+      error {
+        message
+      }
+    }
+  }
+`
 export default class SubmitReviewButton extends Component {
   render() {
     // add validation
