aafc784fa0fddd8ddc8fcb6f27e49263eb247909
Michele Wang <mwang@Michele-Wangs-MacBook-Pro.local>
2018-07-27 12:27:01 -0400

    Add message to screen if you are out of matches

--- src/screens/SocietyScreen/index.js
+++ src/screens/SocietyScreen/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import gql from 'graphql-tag'
 import { Query } from 'react-apollo'
-import { Dimensions, Text } from 'react-native'
+import { Dimensions, Text, View } from 'react-native'
 import SocietyHeader from './components/SocietyHeader'
 import CardSwiper from './components/CardSwiper'
 import { SwiperContainer, Background, CardContainer, Screen } from './styles'
@@ -77,13 +77,20 @@ class SocietyScreen extends Component {
                 if (loading) return <Text>`Loading...`</Text>
                 if (error) return <Text>`Error! ${error.message}`</Text>
                 return (
-                  <SwiperContainer>
-                    <CardSwiper
-                      navigation={this.props.navigation}
-                      width={width}
-                      userData={data.societyQuery}
-                    />
-                  </SwiperContainer>
+                  <View>
+                    {data.societyQuery.length > 0 && (
+                      <SwiperContainer>
+                        <CardSwiper
+                          navigation={this.props.navigation}
+                          width={width}
+                          userData={data.societyQuery}
+                        />
+                      </SwiperContainer>
+                    )}
+                    {data.societyQuery.length === 0 && (
+                      <Text>You ran out of matches! </Text>
+                    )}
+                  </View>
                 )
               }}
             </Query>
