47edaf22e1fb364056df280e0caf1d3496c65e51
Yuke Zheng <yukezheng@Yukes-MacBook-Pro-2.local>
2018-07-27 11:47:36 -0400

    add settings screens to profile screen

--- src/screens/ProfileScreen/components/SettingsMenu/index.js
+++ src/screens/ProfileScreen/components/SettingsMenu/index.js
@@ -1,14 +1,11 @@
 import React, { Component } from 'react'
 import {
-  Container,
-  Menu,
   MenuItemList,
   EmailAddress,
   CancelMembershipButton,
   ButtonText,
-} from './styles'
+} from '../../styles'
 import NavigationArrow from 'react-native-vector-icons/EvilIcons'
-import SettingsHeader from '../SettingsHeader'
 import SettingsMenuItem from '../SettingsMenuItem'
 import Switch from 'react-native-switch-pro'
 
@@ -25,35 +22,30 @@ const MenuItemWithNavigation = ({ title, onPress }) => (
 
 export default class SettingsMenu extends Component {
   render() {
-    const { hideSettings, email } = this.props
+    const { email, onPress } = this.props
     return (
-      <Container>
-        <Menu>
-          <SettingsHeader hideSettings={hideSettings} />
-          <MenuItemList>
-            <SettingsMenuItem title="Email Address" disabled>
-              <EmailAddress>{email}</EmailAddress>
-            </SettingsMenuItem>
-            <SettingsMenuItem title="Enable Email Notifications" disabled>
-              <Switch backgroundActive="rgb(250,53,121)" />
-            </SettingsMenuItem>
-            <SettingsMenuItem title="Recieve Monthly Newsletter" disabled>
-              <Switch backgroundActive="rgb(250,53,121)" />
-            </SettingsMenuItem>
-            <MenuItemWithNavigation title="Password" />
-            <MenuItemWithNavigation title="About" />
-            <MenuItemWithNavigation title="Terms Of Service" />
-            <MenuItemWithNavigation title="Privacy Policy" />
-            <MenuItemWithNavigation title="FAQ's" />
-            <MenuItemWithNavigation title="Contact Us" />
-            <MenuItemWithNavigation title="Restore Purchase" />
-            <MenuItemWithNavigation title="Logout" />
-            <CancelMembershipButton>
-              <ButtonText>CANCEL MEMBERSHIP</ButtonText>
-            </CancelMembershipButton>
-          </MenuItemList>
-        </Menu>
-      </Container>
+      <MenuItemList showsVerticalScrollIndicator={false}>
+        <SettingsMenuItem title="Email Address" disabled>
+          <EmailAddress>{email}</EmailAddress>
+        </SettingsMenuItem>
+        <SettingsMenuItem title="Enable Email Notifications" disabled>
+          <Switch backgroundActive="rgb(250,53,121)" />
+        </SettingsMenuItem>
+        <SettingsMenuItem title="Recieve Monthly Newsletter" disabled>
+          <Switch backgroundActive="rgb(250,53,121)" />
+        </SettingsMenuItem>
+        <MenuItemWithNavigation title="Password" />
+        <MenuItemWithNavigation title="About" />
+        <MenuItemWithNavigation title="Terms Of Service" />
+        <MenuItemWithNavigation title="Privacy Policy" />
+        <MenuItemWithNavigation title="FAQ's" />
+        <MenuItemWithNavigation title="Contact Us" />
+        <MenuItemWithNavigation title="Restore Purchase" />
+        <MenuItemWithNavigation title="Logout" onPress={() => onPress(1)} />
+        <CancelMembershipButton>
+          <ButtonText>CANCEL MEMBERSHIP</ButtonText>
+        </CancelMembershipButton>
+      </MenuItemList>
     )
   }
 }
