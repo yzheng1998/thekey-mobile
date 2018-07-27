e2a93f010219353768a13bd5bd21fc495d0561a9
Michele Wang <mwang@Michele-Wangs-MacBook-Pro.local>
2018-07-27 14:40:25 -0400

    Added default picture for company cards

--- src/components/CompanyCard/index.js
+++ src/components/CompanyCard/index.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 import { Image } from 'react-native'
 import { Card, LeftContainer, Title } from './styles'
 import Rating from './components/RatingStar'
-import DevLogo from '../../../assets/devlogo.png'
 
 export default class CompanyCard extends Component {
   render() {
@@ -19,7 +18,14 @@ export default class CompanyCard extends Component {
         }
       >
         <Image
-          source={picture ? { uri: picture } : DevLogo}
+          source={
+            picture
+              ? { uri: picture }
+              : {
+                  uri:
+                    'https://images.unsplash.com/photo-1486108334972-f02b6c78ba07?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=7b5a12ea524ae41d923b50f2e43f1cb8&auto=format&fit=crop&w=1500&q=80',
+                }
+          }
           style={{ width: 46, height: 46 }}
         />
         <LeftContainer>
