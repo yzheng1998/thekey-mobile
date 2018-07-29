90c2769297cdcc7aab796af8e56482463359ca8c
Diego Covarrubias <diegocovarrubias@Diegos-MacBook-Pro.local>
2018-07-29 19:26:40 -0400

    Add the header background image to assets, so we don't rely on a url

--- src/screens/EventsScreen/components/EventsHeader/index.js
+++ src/screens/EventsScreen/components/EventsHeader/index.js
@@ -10,17 +10,13 @@ import {
 } from './styles'
 import SearchFilterTab from '../../../../components/SearchFilterTab'
 import BackArrow from 'react-native-vector-icons/Ionicons'
+import headerBackgroundImage from '../../../../../assets/headerBackground.jpeg'
 
 export default class EventsHeader extends Component {
   render() {
     const { updateState, selectedIndex } = this.props
     return (
-      <BackgroundImage
-        source={{
-          uri:
-            'https://images.unsplash.com/photo-1503238774835-1e800884d53b?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=e47ac33daae3f0b9c2add178ea367801&auto=format&fit=crop&w=1950&q=80',
-        }}
-      >
+      <BackgroundImage source={headerBackgroundImage}>
         <Tint>
           <SafeView>
             <HeaderContainer>
