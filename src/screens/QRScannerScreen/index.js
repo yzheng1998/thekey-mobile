3a59e71aca29d869a789b15731c50144f7435272
Diego Covarrubias <diegocovarrubias@Diegos-MacBook-Pro.local>
2018-07-29 15:11:52 -0400

    Initial Implementation of the sendFriendRequest functionality from QR Code

--- src/screens/QRScannerScreen/index.js
+++ src/screens/QRScannerScreen/index.js
@@ -1,32 +1,72 @@
 import React, { Component } from 'react'
-import { View } from 'react-native'
+import { View, Alert } from 'react-native'
 import { RNCamera } from 'react-native-camera'
+import { Mutation } from 'react-apollo'
+import gql from 'graphql-tag'
+import { NavigationActions, StackActions } from 'react-navigation'
 
+const SEND_FRIEND_REQUEST = gql`
+  mutation createFriendRequest($recipientId: ID!, $swipedLeft: Boolean!) {
+    createFriendRequest(recipientId: $recipientId, swipedLeft: $swipedLeft) {
+      friendRequest {
+        id
+        sender {
+          id
+        }
+        recipient {
+          id
+          firstName
+          lastName
+        }
+        status
+      }
+    }
+  }
+`
 class QRScannerScreen extends Component {
   render() {
+    const resetAction = StackActions.reset({
+      index: 1,
+      actions: [
+        NavigationActions.navigate({ routeName: 'Discover' }),
+        NavigationActions.navigate({ routeName: 'Profile' }),
+      ],
+    })
     return (
-      <View
-        style={{
-          flex: 1,
-          flexDirection: 'row',
-        }}
+      <Mutation
+        mutation={SEND_FRIEND_REQUEST}
+        onCompleted={() => this.props.navigation.dispatch(resetAction)}
       >
-        <RNCamera
-          barCodeTypes={[RNCamera.Constants.BarCodeType.qr]}
-          flashMode={RNCamera.Constants.FlashMode.on}
-          style={{
-            flex: 1,
-            justifyContent: 'flex-end',
-            alignItems: 'center',
-          }}
-          onBarCodeRead={qrCode => {
-            this.props.sendFriendRequest(qrCode.data)
-          }}
-          ref={cam => {
-            this.camera = cam
-          }}
-        />
-      </View>
+        {(createFriendRequest, { error }) => (
+          <View
+            style={{
+              flex: 1,
+              flexDirection: 'row',
+            }}
+          >
+            {error && Alert.alert('Error', 'Could not read QR code')}
+            <RNCamera
+              barCodeTypes={[RNCamera.Constants.BarCodeType.qr]}
+              flashMode={RNCamera.Constants.FlashMode.on}
+              style={{
+                flex: 1,
+                justifyContent: 'flex-end',
+                alignItems: 'center',
+              }}
+              onBarCodeRead={qrCode => {
+                const variables = {
+                  recipientId: qrCode.data,
+                  swipedLeft: false,
+                }
+                createFriendRequest({ variables })
+              }}
+              ref={cam => {
+                this.camera = cam
+              }}
+            />
+          </View>
+        )}
+      </Mutation>
     )
   }
 }
