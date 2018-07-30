f8a4225d923e9afa9f95abf118fb2693fa541a5d
Michele Wang <mwang@Michele-Wangs-MacBook-Pro.local>
2018-07-30 10:05:35 -0400

    Added Header component for Edit Screens (slightly modified styles from the one in src/components

---
+++ src/screens/EditProfileScreen/components/Header/styles.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components'
+import { themeGet } from 'styled-system'
+
+export const Container = styled.View`
+  width: 100%;
+  background-color: ${themeGet('colors.background')};
+  padding-top: 40px;
+  margin-bottom: 4px;
+`
+export const TitleRow = styled.View`
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 12px;
+`
+export const Title = styled.Text`
+  font-family: ${themeGet('fonts.heavy')};
+  font-size: 18px;
+`
+export const BackButtonContainer = styled.TouchableOpacity`
+  position: absolute;
+  left: 20px;
+  top: -3px;
+`
