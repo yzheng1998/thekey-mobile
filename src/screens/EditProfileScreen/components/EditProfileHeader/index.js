da884133138aab22df6a640e3d8d0efc793ad7f8
Michele Wang <mwang@Michele-Wangs-MacBook-Pro.local>
2018-07-30 11:01:23 -0400

    Incorporate UpdateProfileButton into EditProfileHeader

---
+++ src/screens/EditProfileScreen/components/EditProfileHeader/index.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import {
+  Background,
+  Heading,
+  ButtonContainer,
+  CancelButton,
+  CancelText,
+  SafeView,
+} from './styles'
+import UpdateProfileButton from '../UpdateProfileButton'
+
+export default class EditProfileHeader extends Component {
+  render() {
+    const { goBack, mutationVariables } = this.props
+    return (
+      <SafeView>
+        <Background>
+          <ButtonContainer>
+            <CancelButton onPress={() => goBack}>
+              <CancelText>Cancel</CancelText>
+            </CancelButton>
+            <UpdateProfileButton
+              variables={mutationVariables}
+              goBack={goBack}
+            />
+          </ButtonContainer>
+          <Heading>Edit Profile</Heading>
+        </Background>
+      </SafeView>
+    )
+  }
+}
