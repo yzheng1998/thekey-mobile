da5ff361f827e633f522532e0a243966ad59cd61
Michele Wang <mwang@Michele-Wangs-MacBook-Pro.local>
2018-07-31 16:03:17 -0400

    Added error alert to CreateChatButton

--- src/screens/NetworkScreen/components/CreateChatButton/index.js
+++ src/screens/NetworkScreen/components/CreateChatButton/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { Button, Text } from './styles'
 import { Mutation } from 'react-apollo'
 import gql from 'graphql-tag'
+import { Alert } from 'react-native'
 
 const CREATE_CHAT = gql`
   mutation createChat($participantIds: [ID!]!) {
@@ -28,19 +29,29 @@ export default class CreateChatButton extends Component {
           closeModal()
         }}
       >
-        {createChat => (
-          <Button
-            disabled={disabled}
-            onPress={() => {
-              const variables = {
-                participantIds,
-              }
-              createChat({ variables })
-            }}
-          >
-            <Text disabled={disabled}>Next</Text>
-          </Button>
-        )}
+        {(createChat, { error }) => {
+          if (error) {
+            Alert.alert(
+              'Failed to create chat',
+              'There was an error starting a new chat. Please try again.',
+              [{ text: 'OK', onPress: () => {} }],
+              { cancelable: true },
+            )
+          }
+          return (
+            <Button
+              disabled={disabled}
+              onPress={() => {
+                const variables = {
+                  participantIds,
+                }
+                createChat({ variables })
+              }}
+            >
+              <Text disabled={disabled}>Next</Text>
+            </Button>
+          )
+        }}
       </Mutation>
     )
   }
