3c7d1d72f744536909a1d12791dec77ccfcd7872
Michele Wang <mwang@Michele-Wangs-MacBook-Pro.local>
2018-07-31 15:07:09 -0400

    add error message to AddReviewModal

--- src/screens/AddCompanyReviewModal/components/SubmitReviewButton/index.js
+++ src/screens/AddCompanyReviewModal/components/SubmitReviewButton/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { SubmitButton, SubmitButtonText } from './styles'
-import { Text } from 'react-native'
+import { Text, Alert } from 'react-native'
 import gql from 'graphql-tag'
 import { Mutation } from 'react-apollo'
 
@@ -66,7 +66,15 @@ export default class SubmitReviewButton extends Component {
         }}
       >
         {(addCompanyReview, { error }) => {
-          if (error) return <Text>{error.message}</Text>
+          if (error) {
+            Alert.alert(
+              'Review Submission Failed',
+              'There was an error submitting your review. Please try again.',
+              [{ text: 'OK', onPress: () => {} }],
+              { cancelable: true },
+            )
+          }
+
           return (
             <SubmitButton
               disabled={!acceptedTerms}
