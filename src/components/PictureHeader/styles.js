f7648f704543edcfffe09aae47f190d9b3392a80
Michele Wang <mwang@Michele-Wangs-MacBook-Pro.local>
2018-08-01 13:18:10 -0400

    Fix height of PictureHeader to display properly

--- src/components/PictureHeader/styles.js
+++ src/components/PictureHeader/styles.js
@@ -17,7 +17,7 @@ export const SafeView = styled.SafeAreaView`
 `
 export const BackgroundPicture = styled.ImageBackground`
   width: 100%;
-  height: 340px;
+  height: 365px;
   align-items: center;
   justify-content: center;
   border-top-left-radius: 20px;
