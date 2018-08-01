9daa7a013c508b304e17004f51720db6626f1b4f
Michele Wang <mwang@Michele-Wangs-MacBook-Pro.local>
2018-08-01 12:24:22 -0400

    Add SafeAreaView to GenderScreen

--- src/screens/GenderScreen/index.js
+++ src/screens/GenderScreen/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { View } from 'react-native'
+import { View, SafeAreaView } from 'react-native'
 import {
   ScreenContainer,
   SubtitleView,
@@ -30,37 +30,40 @@ export default class GenderScreen extends Component {
     const userInfo = this.props.navigation.getParam('userInfo')
     return (
       <ScreenContainer>
-        <Header
-          title={`${nodeEmoji.get('wave')} Hi, ${userInfo.firstName}!`}
-          showBack
-          onBackPress={() => this.props.navigation.goBack()}
-          progress="28.5%"
-        />
-        <SubtitleView>
-          <Subtitle>
-            Select the following that apply to your gender identity & experience
-          </Subtitle>
-        </SubtitleView>
-        <View>
-          {genderOptions.map(option => (
-            <GenderButton
-              key={uuidv4()}
-              clicked={option.value === this.state.gender}
-              onPress={() => this.setState({ gender: option.value })}
-            >
-              <ButtonText>{option.label}</ButtonText>
-            </GenderButton>
-          ))}
-        </View>
-        <RegisterButton
-          buttonText="NEXT"
-          disabled={disabled}
-          onPress={() =>
-            this.props.navigation.navigate('YourEducation', {
-              userInfo: { ...userInfo, gender: this.state.gender },
-            })
-          }
-        />
+        <SafeAreaView style={{ flex: 1, backgroundColor: 'white' }}>
+          <Header
+            title={`${nodeEmoji.get('wave')} Hi, ${userInfo.firstName}!`}
+            showBack
+            onBackPress={() => this.props.navigation.goBack()}
+            progress="28.5%"
+          />
+          <SubtitleView>
+            <Subtitle>
+              Select the following that apply to your gender identity &
+              experience
+            </Subtitle>
+          </SubtitleView>
+          <View>
+            {genderOptions.map(option => (
+              <GenderButton
+                key={uuidv4()}
+                clicked={option.value === this.state.gender}
+                onPress={() => this.setState({ gender: option.value })}
+              >
+                <ButtonText>{option.label}</ButtonText>
+              </GenderButton>
+            ))}
+          </View>
+          <RegisterButton
+            buttonText="NEXT"
+            disabled={disabled}
+            onPress={() =>
+              this.props.navigation.navigate('YourEducation', {
+                userInfo: { ...userInfo, gender: this.state.gender },
+              })
+            }
+          />
+        </SafeAreaView>
       </ScreenContainer>
     )
   }
