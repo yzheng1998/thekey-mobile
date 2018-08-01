22df48fbc9cfeb1647d5085b0ee072696831a490
Michele Wang <mwang@Michele-Wangs-MacBook-Pro.local>
2018-08-01 15:38:35 -0400

    Add ResetPasswordScreen

---
+++ src/screens/ResetPasswordScreen/styles.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components'
+import { themeGet } from 'styled-system'
+
+export const ScreenContainer = styled.View`
+  flex: 1;
+  padding-left: 12px;
+  padding-right: 12px;
+  background-color: ${themeGet('colors.background')};
+  padding-bottom: 24px;
+`
+export const SubtitleView = styled.View`
+  width: 100%;
+  margin-top: 5px;
+  margin-bottom: 5px;
+  padding-left: 12px;
+  padding-right: 12px;
+`
+
+export const Subtitle = styled.Text`
+  color: ${props =>
+    props.hyperlink
+      ? themeGet('colors.fontButtonTertiary')
+      : themeGet('colors.fontSubtitle.primary')};
+  font-family: ${themeGet('fonts.medium')};
+  font-size: 14px
+  line-height: 20px;
+`
