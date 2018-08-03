2d8bc1ae4102af2851f09e96dded5fb102f00e16
Jake Steeves <jsteeves@college.harvard.edu>
2018-08-03 10:35:47 -0400

    Linked react-native-fbsdk to Android project

--- android/app/src/main/java/com/thekeymobile/MainApplication.java
+++ android/app/src/main/java/com/thekeymobile/MainApplication.java
@@ -3,6 +3,7 @@ package com.thekeymobile;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.rnfs.RNFSPackage;
 import com.RNFetchBlob.RNFetchBlobPackage;
 import net.jodybrewster.linkedinlogin.RNLinkedinLoginPackage;
@@ -33,6 +34,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new FBSDKPackage(),
             new RNFSPackage(),
             new RNFetchBlobPackage(),
             new RNLinkedinLoginPackage(),
