4d070b90e65a55939fe94d4291fd941ac8f695d6
Michele Wang <mwang@Michele-Wangs-MacBook-Pro.local>
2018-08-03 12:02:03 -0400

    Add s3bucket to config

--- config.js
+++ config.js
@@ -4,4 +4,5 @@ module.exports = {
     process.env.REACT_APP_WS_API_URL || 'ws://localhost:8080/subscriptions',
   LinkedInClientID: '7741ysuwp6w3ty',
   LinkedInRedirectUri: 'http://localhost:3000/auth/linkedin',
+  s3Bucket: 'https://s3.amazonaws.com/thekey-events/',
 }
