292f3a94fe4fedc8da2e44e5f4771f5c69b19b0d
Michele Wang <mwang@Michele-Wangs-MacBook-Pro.local>
2018-08-03 15:04:08 -0400

    Added AlertMessage component to show error messages

---
+++ src/screens/LoginScreen/components/AlertMessage/styles.js
@@ -0,0 +1,29 @@
+import styled from 'styled-components'
+import { themeGet } from 'styled-system'
+
+export const Background = styled.View`
+  width: 100%;
+  align-items: center;
+  justify-content: center;
+`
+export const Container = styled.View`
+  width: 83%;
+  height: 27px;
+  border-radius: 10px;
+  margin-bottom: 10px;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  background-color: ${props =>
+    props.isError ? 'rgb(250, 53, 98)' : 'rgb(119, 210, 103)'};
+`
+export const Message = styled.Text`
+  font-family: ${themeGet('fonts.bold')};
+  color: white;
+  font-size: 12px;
+`
+export const IconContainer = styled.View`
+  justify-content: center;
+  align-items: center;
+  margin-right: 4px;
+`
