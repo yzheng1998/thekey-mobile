a47212d61678ea6309e255885e1b1ef9a522aaa1
Diego Covarrubias <diegocovarrubias@Diegos-MacBook-Pro.local>
2018-08-07 15:04:09 -0400

    Fix bug where app crashed in user didn't have a profilePicture

--- src/screens/DiscoverScreen/components/Header/index.js
+++ src/screens/DiscoverScreen/components/Header/index.js
@@ -16,6 +16,9 @@ const GET_VIEWER = gql`
   }
 `
 
+const defaultProfilePicture =
+  'https://images.unsplash.com/photo-1519145897500-869c40ccb024?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=dc363c8e033813d4f7b798846bb13a24&auto=format&fit=crop&w=582&q=80'
+
 export default class Header extends Component {
   static defaultProps = {}
 
@@ -34,11 +37,13 @@ export default class Header extends Component {
         <Query query={GET_VIEWER}>
           {({ data, loading }) => {
             if (loading) return <LoadingWrapper loading />
+            const { profilePicture } = data.viewer
+            const profilePictureUrl = profilePicture || defaultProfilePicture
             return (
               <IconButton
                 onPress={() => this.props.navigation.navigate('Profile')}
               >
-                <Icon source={{ uri: data.viewer.profilePicture }} />
+                <Icon source={{ uri: profilePictureUrl }} />
               </IconButton>
             )
           }}
