8ad7681a44f3fbcacbd79c535076900c914572e4
Michele Wang <mwang@Michele-Wangs-MacBook-Pro.local>
2018-08-07 09:22:22 -0400

    replace header backgrounds on Events,Jobs, Reviews, Society screen with images from their respective Discover Cards

--- src/screens/ReviewsScreen/components/ReviewsHeader/index.js
+++ src/screens/ReviewsScreen/components/ReviewsHeader/index.js
@@ -2,39 +2,34 @@ import React, { Component } from 'react'
 import SearchFilterTab from '../../../../components/SearchFilterTab'
 import {
   BackgroundImage,
-  Tint,
   SafeView,
   HeaderContainer,
   Title,
   BackButtonContainer,
 } from './styles'
 import BackArrow from 'react-native-vector-icons/Ionicons'
-import HeaderBackground from '../../../../../assets/headerBackground.jpeg'
+import ReviewsBackground from '../../../../../assets/ReviewsBackground.png'
 
 export default class ReviewsHeader extends Component {
   render() {
     const { changeTab, selectedIndex } = this.props
     return (
-      <BackgroundImage source={HeaderBackground}>
-        <Tint>
-          <SafeView>
-            <HeaderContainer>
-              <BackButtonContainer
-                onPress={() => this.props.navigation.goBack()}
-              >
-                <BackArrow name="ios-arrow-back" size={27} color="white" />
-              </BackButtonContainer>
-              <Title>Company Reviews</Title>
-              <SearchFilterTab
-                options={['All', 'Highest Rated']}
-                selectedColor="white"
-                color="white"
-                selectedIndex={selectedIndex}
-                updateState={changeTab}
-              />
-            </HeaderContainer>
-          </SafeView>
-        </Tint>
+      <BackgroundImage source={ReviewsBackground}>
+        <SafeView>
+          <HeaderContainer>
+            <BackButtonContainer onPress={() => this.props.navigation.goBack()}>
+              <BackArrow name="ios-arrow-back" size={27} color="white" />
+            </BackButtonContainer>
+            <Title>Reviews</Title>
+            <SearchFilterTab
+              options={['All', 'Highest Rated']}
+              selectedColor="white"
+              color="white"
+              selectedIndex={selectedIndex}
+              updateState={changeTab}
+            />
+          </HeaderContainer>
+        </SafeView>
       </BackgroundImage>
     )
   }
--- src/screens/SocietyScreen/components/SocietyHeader/index.js
+++ src/screens/SocietyScreen/components/SocietyHeader/index.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import {
   SafeView,
-  Tint,
   ButtonRow,
   BackButton,
   SearchButton,
@@ -9,25 +8,23 @@ import {
   BackgroundImage,
 } from './styles'
 import Icon from 'react-native-vector-icons/Ionicons'
-import HeaderBackground from '../../../../../assets/headerBackground.jpeg'
+import SocietyBackground from '../../../../../assets/SocietyBackground.png'
 
 export default class EventsHeader extends Component {
   render() {
     return (
-      <BackgroundImage source={HeaderBackground}>
-        <Tint>
-          <SafeView>
-            <ButtonRow>
-              <BackButton onPress={() => this.props.navigation.goBack()}>
-                <Icon name="ios-arrow-back" color="white" size={30} />
-              </BackButton>
-              <SearchButton onPress={this.props.openModal}>
-                <Icon name="md-search" color="white" size={25} />
-              </SearchButton>
-            </ButtonRow>
-            <Title>The Society</Title>
-          </SafeView>
-        </Tint>
+      <BackgroundImage source={SocietyBackground}>
+        <SafeView>
+          <ButtonRow>
+            <BackButton onPress={() => this.props.navigation.goBack()}>
+              <Icon name="ios-arrow-back" color="white" size={30} />
+            </BackButton>
+            <SearchButton onPress={this.props.openModal}>
+              <Icon name="md-search" color="white" size={25} />
+            </SearchButton>
+          </ButtonRow>
+          <Title>The Society</Title>
+        </SafeView>
       </BackgroundImage>
     )
   }
