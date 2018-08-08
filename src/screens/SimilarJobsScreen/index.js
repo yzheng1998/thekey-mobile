9bc074c2e7bc9fb0149b22b26c4b3f3434631cae
Michele Wang <mwang@Michele-Wangs-MacBook-Pro.local>
2018-08-08 10:38:42 -0400

    Added JobsScreen style header to Similar Jobs Screen

--- src/screens/SimilarJobsScreen/index.js
+++ src/screens/SimilarJobsScreen/index.js
@@ -1,16 +1,12 @@
 import React, { Component } from 'react'
 import { FlatList, ScrollView, View } from 'react-native'
 import JobCard from '../../components/JobCard'
-import {
-  HeaderBackground,
-  Title,
-  BackButtonContainer,
-  CardDivider,
-} from './styles'
+import { CardDivider } from './styles'
 import gql from 'graphql-tag'
 import { Query } from 'react-apollo'
-import BackButton from 'react-native-vector-icons/Ionicons'
 import LoadingWrapper from '../../components/LoadingWrapper'
+import JobsHeaderImage from '../../../assets/JobsBackground.png'
+import SimilarItemsHeader from '../../components/SimilarItemsHeader'
 
 const _ = require('lodash')
 
@@ -46,14 +42,10 @@ class SimilarJobsScreen extends Component {
           }
           return (
             <ScrollView>
-              <HeaderBackground>
-                <BackButtonContainer
-                  onPress={() => this.props.navigation.goBack()}
-                >
-                  <BackButton name="ios-arrow-back" size={33} color="white" />
-                </BackButtonContainer>
-                <Title>Similar Jobs</Title>
-              </HeaderBackground>
+              <SimilarItemsHeader
+                backgroundImage={JobsHeaderImage}
+                title="Similar Jobs"
+              />
               <FlatList
                 keyExtractor={job => job.id}
                 data={data.similarJobs}
--- src/screens/SimilarJobsScreen/styles.js
+++ src/screens/SimilarJobsScreen/styles.js
@@ -4,25 +4,6 @@ import { themeGet } from 'styled-system'
 export const Background = styled.View`
   flex: 1;
 `
-export const HeaderBackground = styled.View`
-  width: 100%;
-  height: 88px;
-  flex-direction: row;
-  align-items: center;
-  justify-content: center;
-  background-color: black;
-`
-export const Title = styled.Text`
-  color: white;
-  font-family: ${themeGet('fonts.heavy')};
-  font-size: 20px;
-  margin-top: 30px;
-`
-export const BackButtonContainer = styled.TouchableOpacity`
-  position: absolute;
-  top: 40px;
-  left: 25px;
-`
 export const CardDivider = styled.View`
   height: 2px;
   background-color: ${themeGet('colors.divider')};
