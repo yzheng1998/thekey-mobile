3ba407720def435ee49fedb8c0fb9be5fe213794
Yuke Zheng <yukezheng@Yukes-MacBook-Pro-2.local>
2018-08-17 16:00:25 -0400

    mutual friends connection row styles

--- src/screens/MemberScreen/components/MutualFriends/index.js
+++ src/screens/MemberScreen/components/MutualFriends/index.js
@@ -1,11 +1,5 @@
 import React, { Component } from 'react'
-import {
-  ConnectionsRowContainer,
-  Avatar,
-  AvatarContainer,
-  Number,
-  Container,
-} from './styles'
+import { ConnectionsRowContainer, Avatar, Number, Container } from './styles'
 
 export default class MutualFriends extends Component {
   static defaultProps = {
@@ -33,30 +27,28 @@ export default class MutualFriends extends Component {
           })
         }
       >
-        <AvatarContainer>
-          {mutualFriends
-            .slice(0, numberOfPics)
-            .map(friend => (
-              <Avatar
-                avatarSize={avatarSize}
-                source={{ uri: friend.profilePicture }}
-                key={friend.id}
-              />
-            ))}
-          {mutualFriends.length > numberOfPics && (
-            <Container>
-              <Avatar
-                avatarSize={avatarSize}
-                source={{
-                  uri:
-                    'https://upload.wikimedia.org/wikipedia/commons/1/1e/A_blank_black_picture.jpg',
-                }}
-                key={Math.random()}
-              />
-              <Number>+{mutualFriends.length - numberOfPics}</Number>
-            </Container>
-          )}
-        </AvatarContainer>
+        {mutualFriends
+          .slice(0, numberOfPics)
+          .map(friend => (
+            <Avatar
+              avatarSize={avatarSize}
+              source={{ uri: friend.profilePicture }}
+              key={friend.id}
+            />
+          ))}
+        {mutualFriends.length > numberOfPics && (
+          <Container>
+            <Avatar
+              avatarSize={avatarSize}
+              source={{
+                uri:
+                  'https://upload.wikimedia.org/wikipedia/commons/1/1e/A_blank_black_picture.jpg',
+              }}
+              key={Math.random()}
+            />
+            <Number>+{mutualFriends.length - numberOfPics}</Number>
+          </Container>
+        )}
       </ConnectionsRowContainer>
     )
   }
--- src/screens/MemberScreen/components/MutualFriends/styles.js
+++ src/screens/MemberScreen/components/MutualFriends/styles.js
@@ -3,28 +3,24 @@ import { themeGet } from 'styled-system'
 
 export const ConnectionsRowContainer = styled.TouchableOpacity`
   flex-direction: row;
-  margin-left: 16;
-  margin-bottom: 19;
-`
-
-export const AvatarContainer = styled.View`
-  flex-direction: row;
-  padding-right: 10px;
+  align-items: center;
+  padding-left: 3px;
 `
 export const Avatar = styled.Image`
   height: ${props => (props.avatarSize ? props.avatarSize : 24)};
   width: ${props => (props.avatarSize ? props.avatarSize : 24)};
-  margin: -3px;
+  margin-left: -3px;
   border-color: white;
   border-width: 1.5;
   border-radius: ${props => (props.avatarSize ? props.avatarSize / 2 : 12)};
 `
 export const Number = styled.Text`
   position: absolute;
+  top: 4px;
+  right: 6px;
   color: white;
   font-size: 12px;
   font-family: ${themeGet('fonts.bold')};
-  padding-top: 2.5px;
 `
 export const Container = styled.View`
   align-items: center;
