027219d890d6f2808154f4132615f0162a06083f
Yuke Zheng <yukezheng@Yukes-MacBook-Pro-2.local>
2018-09-23 13:37:02 -0400

    ehnicity screen

---
+++ src/screens/EthnicitiesScreen/constants.js
@@ -0,0 +1,11 @@
+export const ethnicityOptions = [
+  { label: 'Black / African-American', value: 'BLACK' },
+  { label: 'White', value: 'WHITE' },
+  { label: 'Asian', value: 'ASIAN' },
+  { label: 'Pacific Islander', value: 'PACIFICISLANDER' },
+  { label: 'Middle Eastern', value: 'MIDDLEEASTERN' },
+  { label: 'Native American', value: 'NATIVEAMERICAN' },
+  { label: 'Latino / Hispanic', value: 'HISPANIC' },
+  { label: 'Other', value: 'OTHER' },
+  { label: 'Decline', value: 'DECLINE' },
+]
