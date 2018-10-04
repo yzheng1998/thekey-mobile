bf6f6cac3631ca175355ffd6aaca580741397429
Diego Covarrubias <diegocovarrubias@Diegos-MacBook-Pro-3.local>
2018-10-04 16:32:01 -0400

    Ready for release

--- config.js
+++ config.js
@@ -1,11 +1,12 @@
 module.exports = {
   graphqlUrl:
     process.env.REACT_APP_API_URL ||
-    'https://thekey-api-staging.herokuapp.com/graphql',
+    'https://thekey-api-production.herokuapp.com/graphql',
   graphqlWsUrl:
     process.env.REACT_APP_WS_API_URL ||
-    'wss://thekey-api-staging.herokuapp.com/subscriptions',
+    'wss://thekey-api-production.herokuapp.com/subscriptions',
   LinkedInClientID: '779rrrorh2glbm',
-  LinkedInRedirectUri: 'https://thekey-api-staging.herokuapp.com/auth/linkedin',
-  s3Bucket: 'https://s3.amazonaws.com/thekey-files-staging/',
+  LinkedInRedirectUri:
+    'https://thekey-api-production.herokuapp.com/auth/linkedin',
+  s3Bucket: 'https://s3.amazonaws.com/thekey-files-production/',
 }
