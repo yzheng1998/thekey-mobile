5d6c7982492632d5403b9f127ed30b0e38e1bd41
Yuke Zheng <yukezheng@Yukes-MacBook-Pro-2.local>
2018-10-28 10:06:13 -0400

    add location services

--- App.js
+++ App.js
@@ -5,13 +5,55 @@ import { ThemeProvider } from 'styled-components/native'
 import { client } from './src/apollo'
 import theme from './theme'
 import * as pushNotifications from './src/services/pushNotifications'
+import { AppState, AsyncStorage } from 'react-native'
 
 pushNotifications.configure()
 
 import { Provider } from 'react-redux'
 import store from './src/redux/store'
 
+const getPosition = () => {
+  navigator.geolocation.getCurrentPosition(
+    async position => {
+      await AsyncStorage.setItem(
+        'latitude',
+        JSON.stringify(position.coords.latitude),
+      )
+      await AsyncStorage.setItem(
+        'longitude',
+        JSON.stringify(position.coords.longitude),
+      )
+    },
+    error => this.setState({ error: error.message }),
+    { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
+  )
+}
+
 export default class App extends Component {
+  state = {
+    error: null,
+    appState: AppState.currentState,
+  }
+
+  componentDidMount() {
+    AppState.addEventListener('change', this.handleAppStateChange)
+    getPosition()
+  }
+
+  componentWillUnmount() {
+    AppState.removeEventListener('change', this.handleAppStateChange)
+  }
+
+  handleAppStateChange = nextAppState => {
+    if (
+      this.state.appState.match(/inactive|background/) &&
+      nextAppState === 'active'
+    ) {
+      getPosition()
+    }
+    this.setState({ appState: nextAppState })
+  }
+
   render() {
     return (
       <Provider store={store}>
