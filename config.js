cf923de61305b274c76feef59c64dbd975bef5aa
Diego Covarrubias <diegocovarrubias@Diegos-MacBook-Pro-3.local>
2018-11-01 16:47:38 -0400

    Ready for Upload to AppStore

--- config.js
+++ config.js
@@ -1,10 +1,14 @@
 module.exports = {
-  graphqlUrl: process.env.REACT_APP_API_URL || 'http://localhost:5000/graphql',
+  graphqlUrl:
+    process.env.REACT_APP_API_URL ||
+    'https://thekey-api-production.herokuapp.com/graphql',
   graphqlWsUrl:
-    process.env.REACT_APP_WS_API_URL || 'ws://localhost:5000/subscriptions',
+    process.env.REACT_APP_WS_API_URL ||
+    'wss://thekey-api-production.herokuapp.com/subscriptions',
   LinkedInClientID: '779rrrorh2glbm',
-  LinkedInRedirectUri: 'https://thekey-api-staging.herokuapp.com/auth/linkedin',
-  s3Bucket: 'https://s3.amazonaws.com/thekey-events/',
+  LinkedInRedirectUri:
+    'https://thekey-api-production.herokuapp.com/auth/linkedin',
+  s3Bucket: 'https://s3.amazonaws.com/thekey-files-production/',
   geocoderApiKey: 'AIzaSyCVvYisZmFqXG0cFD9B6r8utyxnQyzo0r4',
   reviewableCompanyLimit: 30,
   reviewsLimit: 30,
@@ -14,5 +18,5 @@ module.exports = {
   similarJobsLimit: 20,
   chatLimit: 10,
   messageLimit: 15,
-  societySearchLimit: 10,
+  societySearchLimit: 20,
 }
