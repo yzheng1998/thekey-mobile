611bcb6bf98e7d4f112c2a995bc574ebdae085db
Yuke Zheng <yukezheng@Yukes-MacBook-Pro-2.local>
2018-11-10 10:43:46 -0500

    fix verification row with new package

--- src/components/VerificationRow/index.js
+++ src/components/VerificationRow/index.js
@@ -1,65 +1,23 @@
 import React, { Component } from 'react'
-import { ContainerRow, Container, Number } from './styles'
+import { containerStyle, codeInputStyle } from './styles'
+import CodeInput from 'react-native-confirmation-code-input'
 
 export default class VerificationRow extends Component {
-  focusNextField(nextField) {
-    this[nextField].focus()
-  }
   render() {
-    const { onChangeText, onSubmit } = this.props
+    const { compareWithCode, onSubmit } = this.props
     return (
-      <ContainerRow>
-        <Container>
-          <Number
-            autoFocus
-            returnKeyType="next"
-            onChangeText={text => {
-              onChangeText({ code1: text })
-              if (text) this.secondInput.focus()
-            }}
-            maxLength={1}
-          />
-        </Container>
-        <Container>
-          <Number
-            returnKeyType="next"
-            maxLength={1}
-            innerRef={input => {
-              this.secondInput = input
-            }}
-            onChangeText={text => {
-              onChangeText({ code2: text })
-              if (text) this.thirdInput.focus()
-            }}
-          />
-        </Container>
-        <Container>
-          <Number
-            returnKeyType="next"
-            maxLength={1}
-            innerRef={input => {
-              this.thirdInput = input
-            }}
-            onChangeText={text => {
-              onChangeText({ code3: text })
-              if (text) this.fourthInput.focus()
-            }}
-          />
-        </Container>
-        <Container>
-          <Number
-            returnKeyType="done"
-            maxLength={1}
-            innerRef={input => {
-              this.fourthInput = input
-            }}
-            onChangeText={text => {
-              onChangeText({ code4: text })
-              if (text) onSubmit()
-            }}
-          />
-        </Container>
-      </ContainerRow>
+      <CodeInput
+        codeLength={4}
+        keyboardType="numeric"
+        compareWithCode={compareWithCode}
+        activeColor="rgb(255, 90, 83)"
+        inactiveColor="rgb(229, 223, 242)"
+        inputPosition="center"
+        size={62}
+        onFulfill={isValid => onSubmit(isValid)}
+        containerStyle={containerStyle}
+        codeInputStyle={codeInputStyle}
+      />
     )
   }
 }
